0451d62f3f4e185a64f3d1da9f3138ca23040d97
Ian Preston <ianopolous@gmail.com>
2016-10-23 00:29:49 +0100

    send follow request typeahead kinda working

--- src/components/social/index.js
+++ src/components/social/index.js
@@ -2,13 +2,57 @@ module.exports = {
     template: require('social.html'),
     data: function() {
         return {
-	    targetUsername: ""
+	    targetUsername: "",
+	    initialized: false
 	}
     },
     props: ['show', 'data'],
     created: function() {
     },
     methods: {
+	setTypeAhead: function() {
+	    var substringMatcher = function(strs) {
+		return function findMatches(q, cb) {
+                    var matches, substringRegex;
+		    
+                    //an array that will be populated with substring matches
+                    matches = [];
+		
+                    // regex used to determine if a string contains the substring `q`
+                    substrRegex = new RegExp(q, 'i');
+		    
+                    // iterate through the pool of strings and for any string that
+                    // contains the substring `q`, add it to the `matches` array
+                    $.each(strs, function(i, str) {
+			if (substrRegex.test(str)) {
+                            matches.push(str);
+			}
+                    });
+		    
+                    cb(matches);
+		};
+            };
+            const usernames =  this.usernames;
+	    $('#friend-name-input')
+		.typeahead(
+		    {
+			hint: true,
+			highlight: true,
+			minLength: 1
+		    },
+		    {
+			name: 'usernames',
+			source: substringMatcher(usernames)
+		    });
+	},
+	
+	keypress: function() {
+	    if (!this.initialized) {
+		this.setTypeAhead();
+		this.initialized = true;
+	    }
+	},
+	
 	sendInitialFollowRequest: function(name) {
 	    console.log("sending follow request to " + name);
 	    this.data.context.sendInitialFollowRequest(name)
@@ -59,7 +103,9 @@ module.exports = {
     },
     computed: {
 	usernames: function() {
-	    return data.network.usernames.toArray([]);
+	    if (this.data.network == null)
+		return [];
+	    return this.data.network.usernames.toArray([]);
 	}
     }
 }
