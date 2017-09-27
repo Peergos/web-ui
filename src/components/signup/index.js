aefed8edf5bbc6aac84c7f1e31c56714e141cb2c
Chris <chris@boddy.im>
2017-09-27 19:25:52 +0100

    whtespace fixes

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -4,22 +4,22 @@ module.exports = {
     template: require('signup.html'),
     data: function() {
         return {
-	    crypto: null,
-	    network: null,
+            crypto: null,
+            network: null,
             username: [],
             password1: [],
-	    password2: [],
-	    checkPassword: false,
-	    isError: false,
-	    errorClass: "",
-	    error:"",
-	    passwordWarningThreshold: 12,
-	    commonPasswords: commonPasswords,
-	    email: [],
-	    showSpinner: false,
-        errorTitle:'',
-        errorBody:'',
-        showError:false,
+            password2: [],
+            checkPassword: false,
+            isError: false,
+            errorClass: "",
+            error:"",
+            passwordWarningThreshold: 12,
+            commonPasswords: commonPasswords,
+            email: [],
+            showSpinner: false,
+            errorTitle:'',
+            errorBody:'',
+            showError:false,
         };
     },
     props: {
@@ -27,15 +27,15 @@ module.exports = {
     },
     created: function() {
         console.debug('Signup module created!');
-	var that = this;
-	Vue.nextTick(function() {
-	    if (that.username.length == 0)
-		document.getElementById("username").focus();
-	    else if (that.password1.length == 0)
-		document.getElementById("password1").focus();
-	    else
-		document.getElementById("password2").focus();
-	});
+        var that = this;
+        Vue.nextTick(function() {
+            if (that.username.length == 0)
+                document.getElementById("username").focus();
+            else if (that.password1.length == 0)
+                document.getElementById("password1").focus();
+            else
+                document.getElementById("password2").focus();
+        });
     },
     methods: {
         signup : function() {
@@ -47,56 +47,56 @@ module.exports = {
             } else {
                 this.showSpinner = true;
                 return peergos.shared.user.UserContext.signUp(that.username, that.password1, that.network, that.crypto)
-                .thenApply(function(context) {
-                    that.$dispatch('child-msg', {
-                    view:'filesystem',
-                    props:{context: context}
+                    .thenApply(function(context) {
+                        that.$dispatch('child-msg', {
+                            view:'filesystem',
+                            props:{context: context}
+                        });
+                        console.log("Signing in/up took " + (Date.now()-creationStart)+" mS from function call");
+                        this.showSpinner = false;
+                    }).exceptionally(function(throwable) {
+                        console.log('Error signing up: ' + throwable);
+                        that.errorTitle = 'Error signing up'
+                            that.errorBody = throwable.getMessage();
+                        that.showError = true;
+                        that.showSpinner = false;
                     });
-                    console.log("Signing in/up took " + (Date.now()-creationStart)+" mS from function call");
-                    this.showSpinner = false;
-                }).exceptionally(function(throwable) {
-                  console.log('Error signing up: ' + throwable);
-                  that.errorTitle = 'Error signing up'
-                  that.errorBody = throwable.getMessage();
-                  that.showError = true;
-                  that.showSpinner = false;
-                });
             }
         },
-	validatePassword: function(inFirstField) {
-	    if (inFirstField && !this.checkPassword)
-		return;
-	    // after one failed attempt update the status after each keystroke
-	    var passwd = this.password1;
-	    var index = this.commonPasswords.indexOf(passwd);
-	    var suffix = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"][(index+1) % 10];
-	    if (index != -1) {
-		this.checkPassword = true;
-		this.isError = true;
+        validatePassword: function(inFirstField) {
+            if (inFirstField && !this.checkPassword)
+                return;
+            // after one failed attempt update the status after each keystroke
+            var passwd = this.password1;
+            var index = this.commonPasswords.indexOf(passwd);
+            var suffix = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"][(index+1) % 10];
+            if (index != -1) {
+                this.checkPassword = true;
+                this.isError = true;
                 this.errorClass = "has-error has-feedback alert alert-danger";
                 this.error = "Warning: your password is the " + (index+1) + suffix + " most common password!";
             } else if (passwd.length < this.passwordWarningThreshold) {
-		this.checkPassword = true;
-		this.isError = true;
+                this.checkPassword = true;
+                this.isError = true;
                 this.errorClass = "has-error has-feedback alert alert-danger";
                 this.error = "Warning: passwords less than "+ this.passwordWarningThreshold +" characters are considered unsafe.";
             }
-	    else {
+            else {
                 this.errorClass = this.checkPassword ? "alert alert-success" : "";
-		this.error = this.checkPassword ? "That's a better password." : "";
-	    }
-	}
+                this.error = this.checkPassword ? "That's a better password." : "";
+            }
+        }
     },
     computed: {
     },
     events: {
-	'parent-msg': function (msg) {
-	    // `this` in event callbacks are automatically bound
-	    // to the instance that registered it
-	    this.username = msg.username;
-	    this.password1 = msg.password;
-	    this.crypto = msg.crypto;
-	    this.network = msg.network;
-	}
+        'parent-msg': function (msg) {
+            // `this` in event callbacks are automatically bound
+            // to the instance that registered it
+            this.username = msg.username;
+            this.password1 = msg.password;
+            this.crypto = msg.crypto;
+            this.network = msg.network;
+        }
     }
 };
