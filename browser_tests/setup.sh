eb0e1659f18833d82dd68e63649ffe438ce50eb3
Chris <chris@boddy.im>
2018-09-05 19:11:19 +0100

    WIP

--- browser_tests/setup.sh
+++ browser_tests/setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# install python package deps
 pip install --user -r requirements.txt
+
+# install  chrome
+curl -o chrome.zip -L  https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots
+unzip chrome.zip
+
+# add to path
+export PATH=$PATH:$PWD/chrome-linux
