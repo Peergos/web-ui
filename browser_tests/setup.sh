462fc02d7eeab2ffbdc7e037f258fedcbf8c2e5e
Chris <chris@boddy.im>
2018-09-06 20:37:07 +0100

    WIP

--- browser_tests/setup.sh
+++ browser_tests/setup.sh
@@ -8,5 +8,5 @@
 pip install --user -r requirements.txt
 
 # install  chrome
-curl -o chrome.zip -L  https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots
+#curl -o chrome.zip -L  https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots
 unzip chrome.zip
