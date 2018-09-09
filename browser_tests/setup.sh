ec119654b21d6907ff066ff987f908effec756d9
Chris <chris@boddy.im>
2018-09-09 10:27:44 +0100

    WIP

--- browser_tests/setup.sh
+++ browser_tests/setup.sh
@@ -3,9 +3,11 @@
 # 
 # This is meant for a CI server agent, not development 
 #
+virtualenv venv
+source venv/bin/activate
 
 # install python package deps
-pip install --user -r requirements.txt
+pip install -r requirements.txt
 
 # install  chrome
 #curl -o chrome.zip -L  https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots
