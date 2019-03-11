cc741005c8f6734935f89e69e04d653d763e3485
Dr Ian Preston <ianopolous@protonmail.com>
2019-03-11 15:10:14 +0000

    reduce default generated password from 8 words to 7 words.
    This still would take $300B and 10 years to break a login.
    Or alternatively, $30B and 100 years.

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -83,7 +83,7 @@ module.exports = {
 	generatePassword() {
 	    var bytes = nacl.randomBytes(16);
 	    var wordIndices = [];
-	    for (var i=0; i < 8; i++)
+	    for (var i=0; i < 7; i++)
 		wordIndices[i] = bytes[2*i]*8 + (bytes[2*i + 1] & 7);
 	    var password = wordIndices.map(j => bip39[j]).join(" ");
 	    this.passwordFieldType = "text";
