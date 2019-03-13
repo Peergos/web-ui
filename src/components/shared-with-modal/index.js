7bdab4e0a1db02aa736bf04191ff1aa9038769fa
kev <kevodwyer@gmail.com>
2019-03-13 17:03:17 +0000

    add waiting spinner to share functions

--- src/components/shared-with-modal/index.js
+++ src/components/shared-with-modal/index.js
@@ -1,11 +1,17 @@
 module.exports = {
     template: require('shared-with-modal.html'),
+    data: function() {
+        return {
+            showSpinner: false
+        }
+    },
     props: ['show', 'data', 'files', 'context', 'forceshared'],
     created: function() {
     },
     methods: {
         close: function () {
             this.show = false;
+            this.showSpinner = false;
         } ,
         unshare : function (targetUsername, sharedWithAccess) {
             if (this.files.length == 0)
@@ -14,15 +20,18 @@ module.exports = {
                 throw "Unimplemented multiple file share call";
 
             var that = this;
+            this.showSpinner = true;
             if(sharedWithAccess == "Read") {
                 this.context.unShareReadAccess(this.files[0], targetUsername)
                     .thenApply(function(b) {
+                        that.showSpinner = false;
                         console.log("unshared read access to " + that.files[0].getFileProperties().name + " with " + targetUsername);
                         that.forceshared++;
                     });
             } else {
                 this.context.unShareWriteAccess(this.files[0], targetUsername)
                     .thenApply(function(b) {
+                        that.showSpinner = false;
                         console.log("unshared write access to " + that.files[0].getFileProperties().name + " with " + targetUsername);
                         that.forceshared++;
                     });
