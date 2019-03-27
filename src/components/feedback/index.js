602412e26b12f58bda86a87b8453cf97f718dba8
Jonathan Budd <jonathan.budd@protonmail.com>
2019-03-27 19:07:53 +0000

    Added spinner and message following successful submission of feedback.

--- src/components/feedback/index.js
+++ src/components/feedback/index.js
@@ -1,15 +1,34 @@
 module.exports = {
     template: require('feedback.html'),
-    props: ['show', 'context'],
+    data: function() {
+        return {
+        showSpinner: false
+        }
+    },
+    props: ['show', 'context', 'messages', 'externalchange'],
     methods: {
         close: function () {
             this.show = false;
         },
-        submitFeedback: function(){
-            var context = this.getContext();
+        showMessage: function(title, body) {
+            this.messages.push({
+                title: title,
+                body: body,
+                show: true
+            });
+        },
+        submitFeedback: function(fb) {
             var formFeedback = document.getElementById("feedback-text").value;
+            var that = this;
             console.log("Feedback to submit: " + formFeedback);
-            return context.submitFeedback(formFeedback).thenApply(x => console.log("Feedback submitted!"));
+            this.showSpinner = true;
+            this.context.submitFeedback(formFeedback)
+                .thenApply(function(success) {
+                    that.showMessage("Feedback submitted!", "Your feedback has been added to a folder shared with Peergos developers.");
+                    that.showSpinner = false;
+                    console.log("Feedback submitted!");
+                    that.externalchange++;
+                });
         },
         getContext: function(){
             var x = this.contextUpdates;
