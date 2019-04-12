7395a2a925da17191a5bef49a0f4d533a42e351e
kev <kevodwyer@gmail.com>
2019-04-12 11:37:01 +0100

    WIP

--- src/components/filesystem/index.js
+++ src/components/filesystem/index.js
@@ -311,7 +311,7 @@ module.exports = {
             var java_reader = new peergos.shared.user.fs.BrowserFileReader(reader);
             var that = this;
             var context = this.getContext();
-            context.uploadFile(this.currentDir, file.name, java_reader, (file.size - (file.size % Math.pow(2, 32)))/Math.pow(2, 32), file.size, false, function(len){
+            context.uploadFile(this.getPath(), this.currentDir, file.name, java_reader, (file.size - (file.size % Math.pow(2, 32)))/Math.pow(2, 32), file.size, false, function(len){
                 progress.done += len.value_0;
                 if (progress.done >= progress.max) {
                     setTimeout(function(){progress.show = false}, 2000);
