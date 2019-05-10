b00d7ccf57f761f6c1e22b277549b9700f78c5dd
Dr Ian Preston <ianopolous@protonmail.com>
2019-05-10 17:22:51 +0100

    limit share with username suggestions to friends

--- src/components/share/index.js
+++ src/components/share/index.js
@@ -2,7 +2,7 @@ module.exports = {
     template: require('share.html'),
     data: function() {
         return {
-            showSpinner: false,
+	    showSpinner: false,
             targetUsername: "",
             sharedWithAccess: "Read",
 	        errorTitle:'',
@@ -35,127 +35,125 @@ module.exports = {
 	    }
 	    return true;
 	},
-
-    shareWith: function(targetUsername, sharedWithAccess) {
-        if (this.files.length == 0)
-            return this.close();
-        if (this.files.length != 1)
-            throw "Unimplemented multiple file share call";
-
-	    if (! this.allowedToShare(this.files[0]))
-		    return;
+	
+	shareWith: function(targetUsername, sharedWithAccess) {
+            if (this.files.length == 0)
+		return this.close();
+            if (this.files.length != 1)
+		throw "Unimplemented multiple file share call";
 	    
-        var that = this;
-        this.showSpinner = true;
-        this.context.getSocialState().thenApply(function(social){
-            var followers = social.followerRoots.keySet().toArray([]);
-            if(followers.indexOf(targetUsername) == -1) {
-                that.showSpinner = false;
-                that.messages.push({
-                    title: "Sharing not possible!",
-                    body: "Please add as a friend first",
-                    show: true
-                });
-                that.close();
-            } else {
-                that.context.sharedWith(that.files[0]).thenApply(function(allSharedWithUsernames){
-                    var read_usernames = allSharedWithUsernames.left.toArray([]);
-                    var edit_usernames = allSharedWithUsernames.right.toArray([]);
-                    if(read_usernames.indexOf(targetUsername) > -1 || edit_usernames.indexOf(targetUsername) > -1) {
-                        that.showSpinner = false;
-                        that.messages.push({
-                            title: "Already shared!",
-                            body: "",
-                            show: true
-                        });
-                    } else {
-                        var filename = that.files[0].getFileProperties().name;
-                        if(sharedWithAccess == "Read") {
-                            that.context.shareReadAccessWith(that.files[0], targetUsername)
-                                .thenApply(function(b) {
-                                    that.showSpinner = false;
-                                    that.messages.push({
-                                        title: "Success!",
-                                        body: "Sharing complete",
-                                        show: true
-                                    });
-                                    that.close();
-                                    console.log("shared read access to " + filename + " with " + targetUsername);
-                                    that.forceshared++;
-                                }).exceptionally(function(throwable) {
-                                    that.showSpinner = false;
-                                    that.errorTitle = 'Error sharing file: ' + filename;
-                                    that.errorBody = throwable.getMessage();
-                                    that.showError = true;
-                                });
-                        } else {
-                            that.context.shareWriteAccessWith(that.files[0], that.parent, targetUsername)
-                                .thenApply(function(b) {
-                                    that.showSpinner = false;
-                                    that.messages.push({
-                                        title: "Success!",
-                                        body: "Sharing complete",
-                                        show: true
-                                    });
-                                    that.close();
-                                    console.log("shared write access to " + filename + " with " + targetUsername);
-                                    that.forceshared++;
-                                }).exceptionally(function(throwable) {
-                                    that.showSpinner = false;
-                                    that.errorTitle = 'Error sharing file: ' + filename;
-                                    that.errorBody = throwable.getMessage();
-                                    that.showError = true;
-                                });
-                        }
-                    }
-                });
-            }
-        });
-    },
-
-    setTypeAhead: function() {
-        var substringMatcher = function(strs) {
-        return function findMatches(q, cb) {
-                var matches, substringRegex;
-
-                //an array that will be populated with substring matches
-                matches = [];
-
-                // regex used to determine if a string contains the substring `q`
-                substrRegex = new RegExp(q, 'i');
-
-                // iterate through the pool of strings and for any string that
-                // contains the substring `q`, add it to the `matches` array
-                $.each(strs, function(i, str) {
-                    if (substrRegex.test(str)) {
-                        matches.push(str);
-                    }
-                });
-
-                cb(matches);
-            };
-        };
-        var usernames = this.usernames;
-        // remove our username
-        usernames.splice(usernames.indexOf(this.context.username), 1);
-        console.log("TYPEAHEAD:");
-        console.log(usernames);
-        $('#friend-name-input')
-            .typeahead(
-                    {
-                        hint: true,
-                        highlight: true,
-                        minLength: 1
-                    },
-                    {
-                        name: 'usernames',
-                        source: substringMatcher(usernames)
+	    if (! this.allowedToShare(this.files[0]))
+		return;
+	    
+            var that = this;
+            this.showSpinner = true;
+            this.context.getSocialState().thenApply(function(social){
+		var followers = social.followerRoots.keySet().toArray([]);
+		if(followers.indexOf(targetUsername) == -1) {
+                    that.showSpinner = false;
+                    that.messages.push({
+			title: "Sharing not possible!",
+			body: "Please add as a friend first",
+			show: true
                     });
-        },
-    },
-    computed: {
-        usernames: function() {
-            return this.context.network.usernames.toArray([]);
-        }
+                    that.close();
+		} else {
+                    that.context.sharedWith(that.files[0]).thenApply(function(allSharedWithUsernames){
+			var read_usernames = allSharedWithUsernames.left.toArray([]);
+			var edit_usernames = allSharedWithUsernames.right.toArray([]);
+			if(read_usernames.indexOf(targetUsername) > -1 || edit_usernames.indexOf(targetUsername) > -1) {
+                            that.showSpinner = false;
+                            that.messages.push({
+				title: "Already shared!",
+				body: "",
+				show: true
+                            });
+			} else {
+                            var filename = that.files[0].getFileProperties().name;
+                            if(sharedWithAccess == "Read") {
+				that.context.shareReadAccessWith(that.files[0], targetUsername)
+                                    .thenApply(function(b) {
+					that.showSpinner = false;
+					that.messages.push({
+                                            title: "Success!",
+                                            body: "Sharing complete",
+                                            show: true
+					});
+					that.close();
+					console.log("shared read access to " + filename + " with " + targetUsername);
+					that.forceshared++;
+                                    }).exceptionally(function(throwable) {
+					that.showSpinner = false;
+					that.errorTitle = 'Error sharing file: ' + filename;
+					that.errorBody = throwable.getMessage();
+					that.showError = true;
+                                    });
+                            } else {
+				that.context.shareWriteAccessWith(that.files[0], that.parent, targetUsername)
+                                    .thenApply(function(b) {
+					that.showSpinner = false;
+					that.messages.push({
+                                            title: "Success!",
+                                            body: "Sharing complete",
+                                            show: true
+					});
+					that.close();
+					console.log("shared write access to " + filename + " with " + targetUsername);
+					that.forceshared++;
+                                    }).exceptionally(function(throwable) {
+					that.showSpinner = false;
+					that.errorTitle = 'Error sharing file: ' + filename;
+					that.errorBody = throwable.getMessage();
+					that.showError = true;
+                                    });
+                            }
+			}
+                    });
+		}
+            });
+	},
+	
+	setTypeAhead: function() {
+            var substringMatcher = function(strs) {
+		return function findMatches(q, cb) {
+                    var matches, substringRegex;
+		    
+                    //an array that will be populated with substring matches
+                    matches = [];
+		    
+                    // regex used to determine if a string contains the substring `q`
+                    substrRegex = new RegExp(q, 'i');
+		    
+                    // iterate through the pool of strings and for any string that
+                    // contains the substring `q`, add it to the `matches` array
+                    $.each(strs, function(i, str) {
+			if (substrRegex.test(str)) {
+                            matches.push(str);
+			}
+                    });
+		    
+                    cb(matches);
+		};
+            };
+
+	    var that = this;
+	    this.context.getSocialState().thenApply(function(social){
+		var friendNames = social.followerRoots.keySet().toArray([]);
+		
+		console.log("Share TYPEAHEAD:");
+		console.log(friendNames);
+		$('#friend-name-input')
+		    .typeahead(
+			{
+                            hint: true,
+                            highlight: true,
+                            minLength: 1
+			},
+			{
+                            name: 'usernames',
+                            source: substringMatcher(friendNames)
+			});
+            })
+	}
     }
 }
