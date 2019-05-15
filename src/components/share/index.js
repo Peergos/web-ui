045e1b506f9f76903d4b0472c4a0373d084308c2
kev <kevodwyer@gmail.com>
2019-05-15 17:55:09 +0100

    show error if unsharing fails

--- src/components/share/index.js
+++ src/components/share/index.js
@@ -2,10 +2,10 @@ module.exports = {
     template: require('share.html'),
     data: function() {
         return {
-	    showSpinner: false,
+	        showSpinner: false,
             targetUsername: "",
             sharedWithAccess: "Read",
-	    errorTitle:'',
+	        errorTitle:'',
             errorBody:'',
             showError:false
         }
@@ -22,95 +22,95 @@ module.exports = {
 
 	allowedToShare: function(file) {
 	    if (file.isUserRoot()) {
-		this.errorTitle = 'You cannot share your home directory!';
-                this.errorBody = "";
-                this.showError = true;
-		return false;
+            this.errorTitle = 'You cannot share your home directory!';
+            this.errorBody = "";
+            this.showError = true;
+            return false;
 	    }
 	    if (this.sharedWithAccess == "Edit" && file.getOwnerName() != this.context.username) {
-		this.errorTitle = 'Only the owner of a file can grant write access!';
-                this.errorBody = "";
-                this.showError = true;
-		return false;
+            this.errorTitle = 'Only the owner of a file can grant write access!';
+            this.errorBody = "";
+            this.showError = true;
+            return false;
 	    }
 	    return true;
 	},
 	
 	shareWith: function(targetUsername, sharedWithAccess) {
-            if (this.files.length == 0)
-		return this.close();
-            if (this.files.length != 1)
-		throw "Unimplemented multiple file share call";
+        if (this.files.length == 0)
+		    return this.close();
+        if (this.files.length != 1)
+		    throw "Unimplemented multiple file share call";
 	    
 	    if (! this.allowedToShare(this.files[0]))
-		return;
+		    return;
 	    
-            var that = this;
-            this.showSpinner = true;
-            this.context.getSocialState().thenApply(function(social){
-		var followers = social.followerRoots.keySet().toArray([]);
-		if(followers.indexOf(targetUsername) == -1) {
-                    that.showSpinner = false;
-                    that.messages.push({
-			title: "Sharing not possible!",
-			body: "Please add as a friend first",
-			show: true
-                    });
-                    that.close();
-		} else {
-                    that.context.sharedWith(that.files[0]).thenApply(function(allSharedWithUsernames){
-			var read_usernames = allSharedWithUsernames.left.toArray([]);
-			var edit_usernames = allSharedWithUsernames.right.toArray([]);
-			if(read_usernames.indexOf(targetUsername) > -1 || edit_usernames.indexOf(targetUsername) > -1) {
-                            that.showSpinner = false;
-                            that.messages.push({
-				title: "Already shared!",
-				body: "",
-				show: true
+        var that = this;
+        this.showSpinner = true;
+        this.context.getSocialState().thenApply(function(social){
+            var followers = social.followerRoots.keySet().toArray([]);
+            if(followers.indexOf(targetUsername) == -1) {
+                that.showSpinner = false;
+                that.messages.push({
+                    title: "Sharing not possible!",
+                    body: "Please add as a friend first",
+                    show: true
+                });
+                that.close();
+            } else {
+                that.context.sharedWith(that.files[0]).thenApply(function(allSharedWithUsernames){
+                    var read_usernames = allSharedWithUsernames.left.toArray([]);
+                    var edit_usernames = allSharedWithUsernames.right.toArray([]);
+                    if(read_usernames.indexOf(targetUsername) > -1 || edit_usernames.indexOf(targetUsername) > -1) {
+                        that.showSpinner = false;
+                        that.messages.push({
+                            title: "Already shared!",
+                            body: "",
+                            show: true
+                        });
+                    } else {
+                        var filename = that.files[0].getFileProperties().name;
+                        if(sharedWithAccess == "Read") {
+                            that.context.shareReadAccessWith(that.files[0], targetUsername)
+                            .thenApply(function(b) {
+                                that.showSpinner = false;
+                                that.messages.push({
+                                    title: "Success!",
+                                    body: "Sharing complete",
+                                    show: true
+                                });
+                                that.close();
+                                console.log("shared read access to " + filename + " with " + targetUsername);
+                                that.forceshared++;
+                            }).exceptionally(function(throwable) {
+                                that.showSpinner = false;
+                                that.errorTitle = 'Error sharing file: ' + filename;
+                                that.errorBody = throwable.getMessage();
+                                that.showError = true;
                             });
-			} else {
-                            var filename = that.files[0].getFileProperties().name;
-                            if(sharedWithAccess == "Read") {
-				that.context.shareReadAccessWith(that.files[0], targetUsername)
-                                    .thenApply(function(b) {
-					that.showSpinner = false;
-					that.messages.push({
-                                            title: "Success!",
-                                            body: "Sharing complete",
-                                            show: true
-					});
-					that.close();
-					console.log("shared read access to " + filename + " with " + targetUsername);
-					that.forceshared++;
-                                    }).exceptionally(function(throwable) {
-					that.showSpinner = false;
-					that.errorTitle = 'Error sharing file: ' + filename;
-					that.errorBody = throwable.getMessage();
-					that.showError = true;
-                                    });
-                            } else {
-				that.context.shareWriteAccessWith(that.files[0], that.parent, targetUsername)
-                                    .thenApply(function(b) {
-					that.showSpinner = false;
-					that.messages.push({
-                                            title: "Success!",
-                                            body: "Sharing complete",
-                                            show: true
-					});
-					that.close();
-					console.log("shared write access to " + filename + " with " + targetUsername);
-					that.forceshared++;
-                                    }).exceptionally(function(throwable) {
-					that.showSpinner = false;
-					that.errorTitle = 'Error sharing file: ' + filename;
-					that.errorBody = throwable.getMessage();
-					that.showError = true;
-                                    });
-                            }
-			}
-                    });
-		}
-            });
+                        } else {
+                            that.context.shareWriteAccessWith(that.files[0], that.parent, targetUsername)
+                            .thenApply(function(b) {
+                                that.showSpinner = false;
+                                that.messages.push({
+                                    title: "Success!",
+                                    body: "Sharing complete",
+                                    show: true
+                                });
+                                that.close();
+                                console.log("shared write access to " + filename + " with " + targetUsername);
+                                that.forceshared++;
+                            }).exceptionally(function(throwable) {
+                                that.showSpinner = false;
+                                that.errorTitle = 'Error sharing file: ' + filename;
+                                that.errorBody = throwable.getMessage();
+                                that.showError = true;
+                            });
+                        }
+                    }
+                });
+            }
+        });
 	},
 	
 	setTypeAhead: function() {
