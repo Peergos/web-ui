dc701c3f19f3c3c10b10e8806b7682ead386a813
Dr Ian Preston <ianopolous@protonmail.com>
2019-06-06 13:33:06 +0100

    add password warnings to change password panel

--- src/components/password/index.js
+++ src/components/password/index.js
@@ -3,8 +3,12 @@ module.exports = {
     data: function() {
         return {
             existing: "",
-            pw1: "",
-            pw2: "",
+            password1: "",
+            password2: "",
+            passwordFieldType: "password",
+	    password2FieldType: "password",
+	    passwordWarningThreshold: 12,
+            checkPassword: false,
             error: "",
             isError:false,
             errorClass: ""
@@ -14,14 +18,46 @@ module.exports = {
     created: function() {
     },
     methods: {
-        updatePassword: function () {
-            if(this.existing.length == 0 || this.pw1.length == 0 || this.pw2.length == 0) {
+        togglePassword1: function() {
+	    this.passwordFieldType = this.passwordFieldType == "text" ? "password" : "text";
+	},
+	
+        togglePassword2: function() {
+	    this.password2FieldType = this.password2FieldType == "text" ? "password" : "text";
+	},
+	
+        validatePassword: function(inFirstField) {
+            if (inFirstField && !this.checkPassword)
+                return;
+            // after one failed attempt update the status after each keystroke
+            var passwd = this.password1;
+            var index = this.commonPasswords.indexOf(passwd);
+            var suffix = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"][(index+1) % 10];
+            if (index != -1) {
+                this.checkPassword = true;
+                this.isError = true;
+                this.errorClass = "has-error has-feedback alert alert-danger";
+                this.error = "Warning: your password is the " + (index+1) + suffix + " most common password!";
+            } else if (passwd.length < this.passwordWarningThreshold) {
+                this.checkPassword = true;
+                this.isError = true;
+                this.errorClass = "has-error has-feedback alert alert-danger";
+                this.error = "Warning: passwords less than "+ this.passwordWarningThreshold +" characters are considered unsafe.";
+            }
+            else {
+                this.errorClass = this.checkPassword ? "alert alert-success" : "";
+                this.error = this.checkPassword ? "That's a better password." : "";
+            }
+        },
+
+	updatePassword: function () {
+            if(this.existing.length == 0 || this.password1.length == 0 || this.password2.length == 0) {
                 this.isError = true;
                 this.errorClass = "has-error has-feedback alert alert-danger";
                 this.error = "All fields must be populated!";
             } else {
-                if (this.pw1 == this.pw2) {
-                    this.changepassword(this.existing, this.pw1);
+                if (this.password1 == this.password2) {
+                    this.changepassword(this.existing, this.password1);
                     this.show = false;
                 } else {
                     this.isError = true;
