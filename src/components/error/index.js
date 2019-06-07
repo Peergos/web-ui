4f84c9f45c7a0a4dfabbbf450cab57b09b915d84
kev <kevodwyer@gmail.com>
2019-06-07 17:37:51 +0100

    move replace chars to javascript

--- src/components/error/index.js
+++ src/components/error/index.js
@@ -5,7 +5,14 @@ module.exports = {
     },
     methods: {
         decodeError: function(errorBody) {
-            let str = decodeURIComponent(errorBody);
+            let jsErrorBody = errorBody.split("\\+").join("%20")
+                            .split("\\%21").join("!")
+                            .split("\\%27").join("'")
+                            .split("\\%28").join("(")
+                            .split("\\%29").join(")")
+                            .split("\\%7E").join("~");
+
+            let str = decodeURIComponent(jsErrorBody);
             let token = 'java.lang.JsException: ';
             return str.startsWith(token) ? str.substring(token.length) : str;
         },
