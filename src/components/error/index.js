b17b8181124bfa57f5f82d640ba619ae4eb2f550
kev <kevodwyer@gmail.com>
2019-06-07 17:23:08 +0100

    url decode error msgs

--- src/components/error/index.js
+++ src/components/error/index.js
@@ -4,6 +4,11 @@ module.exports = {
     created: function() {
     },
     methods: {
+        decodeError: function(errorBody) {
+            let str = decodeURIComponent(errorBody);
+            let token = 'java.lang.JsException: ';
+            return str.startsWith(token) ? str.substring(token.length) : str;
+        },
         close: function () {
             this.show = false;
         }
