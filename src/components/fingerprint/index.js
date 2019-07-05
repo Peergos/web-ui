bb1ca15fbab2d84f365f7b7b55d71854564c2c44
Dr Ian Preston <ianopolous@protonmail.com>
2019-07-05 21:12:09 +0100

    clean up verification, accessing camera, but can't get image

--- src/components/fingerprint/index.js
+++ src/components/fingerprint/index.js
@@ -15,7 +15,34 @@ module.exports = {
         },
 
 	scanQRCode: function() {
-	    
+	    console.log("Scan QR code");
+	    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+		var video = document.getElementById('video');
+		var that = this;
+		navigator.mediaDevices.getUserMedia({ video: {
+		    width: 512, height:512
+		}}).then(function(stream) {
+		    video.srcObject = stream;
+		    video.play();
+		    that.takeSnapshot()
+		}).catch(function(error) {
+		    console.log("Is webcam connected?");
+		    console.error(error);
+		});
+	    }
+	},
+
+	takeSnapshot: function() {
+	    var canvas = document.getElementById('canvas');
+	    var context = canvas.getContext('2d');
+	    var video = document.getElementById('video');
+	    context.drawImage(video, 0, 0, 640, 480);
+	    var image = this.convertCanvasToImage(canvas)
+	    console.log(image);
+	},
+
+	convertCanvasToImage: function(canvas) {
+	    return canvas.toDataURL("image/png");
 	}
     },
     computed: {
@@ -28,7 +55,10 @@ module.exports = {
 	    var split = [];
 	    for (var i=0; i < res.length; i += 5)
 		split.push(res.substring(i, i + 5))
-            return split.join(" ");
+	    var lines = [];
+	    for (var j=0; j < 3; j++)
+		lines.push(split.slice(j*4, j*4 + 4).join(" "));
+            return lines;
         },
     }
 };
