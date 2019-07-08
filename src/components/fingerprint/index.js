04441227a8d1d1e76fd14559835ba755d26626a9
Dr Ian Preston <ianopolous@protonmail.com>
2019-07-08 23:43:59 +0100

    improve ux around qr code scanning

--- src/components/fingerprint/index.js
+++ src/components/fingerprint/index.js
@@ -4,7 +4,7 @@ module.exports = {
         return {
 	    width: 512,
 	    height: 512,
-	    stream: null
+	    stream: null,
         };
     },
     props: ['fingerprint', 'friendname', 'context'],
@@ -22,63 +22,66 @@ module.exports = {
 	scanQRCode: function() {
 	    console.log("Scan QR code");
 	    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-		var video = document.getElementById('video');
 		var that = this;
 		navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
 		    that.stream = stream;
-		    video.srcObject = stream;
-		    video.play();
-		    that.takeSnapshot(60)
+		    setTimeout(() => {
+			var video = document.getElementById('video');
+			video.srcObject = stream;
+			video.play()
+			that.takeSnapshot(60)
+		    }, 100);
 		}).catch(function(error) {
-		    console.log("Is webcam connected?");
+		    alert("Is webcam connected?");
 		    console.error(error);
+		    that.stream = null;
 		});
 	    }
 	},
 
 	takeSnapshot: function(attemptsLeft) {
-	    var canvas = document.getElementById('canvas');
-	    var vctx = canvas.getContext('2d');
+	    var canvas = document.createElement('canvas');
+	    canvas.width = 512;
+	    canvas.height = 512;
 	    var video = document.getElementById('video');
+	    var vctx = canvas.getContext('2d');
 	    vctx.drawImage(video, 0, 0, this.width, this.height);
-	    this.processSnapshot(attemptsLeft);
+	    this.processSnapshot(attemptsLeft, vctx);
 	},
 
-	processSnapshot: function(attemptsLeft) {
-	    var vctx = canvas.getContext('2d');
+	processSnapshot: function(attemptsLeft, vctx) {
 	    var pixels = this.convertCanvasToPixels(vctx)
 	    try {
 		var scanned = peergos.shared.fingerprint.FingerPrint.decodeFromPixels(pixels, this.width, this.height);
-		console.log("Success!");
-		alert("success!!!!!")
+		this.stream.getVideoTracks()[0].stop();
+		this.stream = null;
+		alert("success scanning qr code!!!!!")
 		var that = this;
 		this.context.generateFingerPrint(this.friendname)
 		    .thenApply(gen => {
-			that.stream.getVideoTracks()[0].stop();
 			if (gen.matches(scanned)) {
-			    alert("match!");
+			    alert("Friend successfully verified!");
 			} else
 			    alert("non matching fingerprint!!");
 		    })
 	    } catch (err) {
-		console.log("Couldn't find qr code in image", err);
+		console.log("Couldn't find qr code in image");
 		if (attemptsLeft > 0)
 		    setTimeout(() => this.takeSnapshot(attemptsLeft-1), 1000);
-		else
+		else {
 		    this.stream.getVideoTracks()[0].stop();
+		    this.stream = null;
+		}
 	    }
 	},
 
 	convertCanvasToPixels: function(context) {
 	    var b = context.getImageData(0, 0, this.width, this.height).data;
-	    console.log("converting")
-	    console.log(b)
 	    // Reverting bytes from RGBA to ARGB
 	    var pixels = []
 	    for (var i=0 ; i < b.length/4 ; i++) {
 		pixels[i] = (b[4*i + 3] << 24) | (b[4*i] << 16) | (b[4*i + 1] << 8) | (b[4*i + 2]);
 	    }
-  	    console.log(pixels);
 	    return pixels;
 	}
     },
