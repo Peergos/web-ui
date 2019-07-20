e277feff59c6fadbe7234b2bd106447bdb6954de
Dr Ian Preston <ianopolous@protonmail.com>
2019-07-20 22:35:50 +0100

    skeleton of app sandbox 1) sandboxed iframe with JS enabled 2) messages being sent between main page and iframe

---
+++ assets/apps/pdf/init.js
@@ -0,0 +1,14 @@
+document.getElementById("subtitle").textContent = "JS is working";
+window.addEventListener('message', function (e) {
+    // You must verify that the origin of the message's sender matches your
+    // expectations. In this case, we're only planning on accepting messages
+    // from our own origin, so we can simply compare the message event's
+    // origin to the location of this document. If we get a message from an
+    // unexpected host, ignore the message entirely.
+    if (e.origin !== (window.location.protocol + "//" + window.location.host))
+        return;
+    
+    var mainWindow = e.source;
+    
+    mainWindow.postMessage("message sent successfully from iframe!", e.origin);
+});
---
+++ src/components/pdf/index.js
@@ -0,0 +1,41 @@
+module.exports = {
+    template: require('pdf.html'),
+    data: function() {
+        return {
+            showSpinner: false,
+        }
+    },
+    props: ['context', 'file'],
+    created: function() {
+        this.startListener();
+    },
+    methods: {
+	startListener: function() {
+	    var iframe = document.getElementById("pdf");
+	    if (iframe == null)
+		setTimeout(this.startListener, 500);
+	    // Listen for response messages from the frames.
+	    window.addEventListener('message', function (e) {
+		// Normally, you should verify that the origin of the message's sender
+		// was the origin and source you expected. This is easily done for the
+		// unsandboxed frame. The sandboxed frame, on the other hand is more
+		// difficult. Sandboxed iframes which lack the 'allow-same-origin'
+		// header have "null" rather than a valid origin. This means you still
+		// have to be careful about accepting data via the messaging API you
+		// create. Check that source, and validate those inputs!
+		if (e.origin === "null" && e.source === iframe.contentWindow) {
+		    alert('Result: ' + e.data);
+		}
+	    });
+	    // Note that we're sending the message to "*", rather than some specific
+            // origin. Sandboxed iframes which lack the 'allow-same-origin' header
+            // don't have an origin which you can target: you'll have to send to any
+            // origin, which might alow some esoteric attacks. Validate your output!
+            iframe.contentWindow.postMessage("Hey", '*');
+	},
+
+        close: function () {
+            this.$emit("hide-pdf-viewer");
+        }
+    },
+}
