6b3637a8ba67f8881e1a1be31d918ced9827ad1d
kevin odwyer <kevodwyer@gmail.com>
2020-03-18 17:17:26 +0000

    fix double opening of apps - first by gallery() then onurlchange()

--- src/components/code-editor/index.js
+++ src/components/code-editor/index.js
@@ -3,8 +3,8 @@ module.exports = {
     data: function() {
         return {
             showSpinner: false,
-	    expectingSave: false,
-	    saving: false,
+	        expectingSave: false,
+	        saving: false
         }
     },
     props: ['context', 'file', 'parent'],
@@ -15,8 +15,8 @@ module.exports = {
 	startListener: function() {
 	    var iframe = document.getElementById("editor");
 	    if (iframe == null) {
-		setTimeout(this.startListener, 500);
-		return;
+    		setTimeout(this.startListener, 500);
+	    	return;
 	    }
 	    var that = this;
 	    // Listen for response messages from the frames.
@@ -30,8 +30,8 @@ module.exports = {
 		// create. Check that source, and validate those inputs!
 		if (e.origin === "null" && e.source === iframe.contentWindow) {
 		    if (that.expectingSave) {
-			that.save(e.data.text);
-			that.expectingSave = false;
+			    that.save(e.data.text);
+			    that.expectingSave = false;
 		    }
 		}
 	    });
@@ -44,75 +44,76 @@ module.exports = {
 	    var mimeType = "text/x-markdown";
 	    var modes = ["markdown"]; // default to markdown for plain text
 	    if (name.endsWith(".java")) {
-		modes = ["clike"];
-		mimeType = "text/x-java";
+		    modes = ["clike"];
+		    mimeType = "text/x-java";
 	    } else if (name.endsWith(".scala")) {
-		modes = ["clike"];
-		mimeType = "text/x-scala";
+		    modes = ["clike"];
+		    mimeType = "text/x-scala";
 	    } else if (name.endsWith(".kt")) {
-		modes = ["clike"];
-		mimeType = "text/x-kotlin";
+		    modes = ["clike"];
+		    mimeType = "text/x-kotlin";
 	    } else if (name.endsWith(".c")) {
-		modes = ["clike"];
-		mimeType = "text/x-csrc";
+		    modes = ["clike"];
+		    mimeType = "text/x-csrc";
 	    } else if (name.endsWith(".cpp")) {
-		modes = ["clike"];
-		mimeType = "text/x-c++src";
+		    modes = ["clike"];
+		    mimeType = "text/x-c++src";
 	    } else if (name.endsWith(".clj")) {
-		modes = ["clojure"];
-		mimeType = "text/x-clojure";
+		    modes = ["clojure"];
+		    mimeType = "text/x-clojure";
 	    } else if (name.endsWith(".css")) {
-		modes = ["css"];
-		mimeType = "text/css";
+		    modes = ["css"];
+		    mimeType = "text/css";
 	    } else if (name.endsWith(".diff")) {
-		modes = ["diff"];
-		mimeType = "text/x-diff";
+		    modes = ["diff"];
+		    mimeType = "text/x-diff";
 	    } else if (name.endsWith(".go")) {
-		modes = ["go"];
-		mimeType = "text/x-go";
+		    modes = ["go"];
+		    mimeType = "text/x-go";
 	    } else if (name.endsWith(".html")) {
-		modes = ["xml", "javascript", "css", "htmlmixed"];
-		mimeType = "text/html";
+		    modes = ["xml", "javascript", "css", "htmlmixed"];
+		    mimeType = "text/html";
 	    } else if (name.endsWith(".js")) {
-		modes = ["javascript"];
-		mimeType = "text/javascript";
+		    modes = ["javascript"];
+		    mimeType = "text/javascript";
 	    } else if (name.endsWith(".json")) {
-		modes = ["javascript"];
-		mimeType = "application/json";
+		    modes = ["javascript"];
+		    mimeType = "application/json";
 	    } else if (name.endsWith(".py")) {
-		modes = ["python"];
-		mimeType = "text/x-python";
+		    modes = ["python"];
+		    mimeType = "text/x-python";
 	    } else if (name.endsWith(".rs")) {
-		modes = ["rust"];
-		mimeType = "text/x-rustsrc";
+		    modes = ["rust"];
+		    mimeType = "text/x-rustsrc";
 	    } else if (name.endsWith(".r")) {
-		modes = ["r"];
-		mimeType = "text/x-rsrc";
+		    modes = ["r"];
+		    mimeType = "text/x-rsrc";
 	    } else if (name.endsWith(".rb")) {
-		modes = ["ruby"];
-		mimeType = "text/x-ruby";
+		    modes = ["ruby"];
+		    mimeType = "text/x-ruby";
 	    } else if (name.endsWith(".sh")) {
-		modes = ["shell"];
-		mimeType = "text/x-sh";
+		    modes = ["shell"];
+		    mimeType = "text/x-sh";
 	    } else if (name.endsWith(".tex")) {
-		modes = ["stex"];
-		mimeType = "text/x-stex";
+		    modes = ["stex"];
+		    mimeType = "text/x-stex";
 	    } else if (name.endsWith(".xml")) {
-		modes = ["xml"];
-		mimeType = "application/xml";
+		    modes = ["xml"];
+		    mimeType = "application/xml";
 	    } else if (name.endsWith(".yaml")) {
-		modes = ["yaml"];
-		mimeType = "text/x-yaml";
+		    modes = ["yaml"];
+		    mimeType = "text/x-yaml";
 	    }
 	    var readOnly = ! this.file.isWritable();
-	    
-	    this.file.getInputStream(this.context.network, this.context.crypto, props.sizeHigh(), props.sizeLow(), function(read){}).thenCompose(function(reader) {
-		var size = that.getFileSize(props);
-		var data = convertToByteArray(new Int8Array(size));
-		return reader.readIntoArray(data, 0, data.length)
-		    .thenApply(function(read){
-			iframe.contentWindow.postMessage({modes:modes, mime:mimeType, readOnly:readOnly, text:new TextDecoder().decode(data)}, '*');
-		    });
+
+	    this.file.getInputStream(this.context.network, this.context.crypto, props.sizeHigh(), props.sizeLow(), function(read){})
+	        .thenCompose(function(reader) {
+                var size = that.getFileSize(props);
+                var data = convertToByteArray(new Int8Array(size));
+                return reader.readIntoArray(data, 0, data.length)
+                    .thenApply(function(read){
+                        iframe.contentWindow.postMessage({modes:modes, mime:mimeType, readOnly:readOnly, text:new TextDecoder().decode(data)}, '*');
+                    });
 	    });
 	},
 
@@ -122,7 +123,7 @@ module.exports = {
 	    iframe.contentWindow.postMessage({type:"save"}, '*');
 	},
 
-        save: function(text) {
+    save: function(text) {
 	    var bytes = convertToByteArray(new TextEncoder().encode(text));
 	    var java_reader = peergos.shared.user.fs.AsyncReader.build(bytes);
 
@@ -135,7 +136,7 @@ module.exports = {
 	    this.parent.uploadFileJS(file.getName(), java_reader, (size - (size % Math.pow(2, 32)))/Math.pow(2, 32), size,
 				true, true, context.network, context.crypto, len => {}, context.getTransactionService())
         .thenApply(function(updated) {
-             that.saving = false;
+            that.saving = false;
 		}).exceptionally(function(throwable) {
 		    console.log('Error uploading file: ' + file.getName());
 		    console.log(throwable.getMessage());
@@ -143,13 +144,13 @@ module.exports = {
 		});
 	},
 
-        close: function () {
-            this.$emit("hide-code-editor");
-        }
+    close: function () {
+        this.$emit("hide-code-editor");
+    }
     },
     computed: {
-	isWritable: function() {
-	    return this.file.isWritable();
-	}
+        isWritable: function() {
+	        return this.file.isWritable();
+	    }
     }
 }
