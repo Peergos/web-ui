5d1139e048e8d7b8c65e665fc02a03e965ee684a
kevin odwyer <kevodwyer@gmail.com>
2020-10-08 17:55:01 +0100

    use new fast method to retrieve shared files from cap

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -34,48 +34,38 @@ module.exports = {
                 this.viewAction(entry.path, entry.name);
             }
         },
-        createTimelineEntry: function(entry) {
-            var future = peergos.shared.util.Futures.incomplete();
-            let that = this;
+        createTimelineEntry: function(entry, file) {
             let info = entry.sharer + " has shared";
             if(entry.cap.isWritable() ) {
                 info = info + " read/write access to";
             } else {
                 info = info + " read access to";
             }
-            this.context.getByPath(entry.path).thenApply(function(file) {
-                if (file.ref == null) {
-                    return;
-                }
-                let props = file.ref.props;
-                if (props.isHidden) {
-                    return;
-                }
-                if (props.isDirectory) {
-                    info = info + " the directory";
-                } else {
-                    info = info + " the file";
-                }
-                if (entry.sharer != entry.owner) {
-                    info = info + " owned by " + entry.owner;
-                }
-                info = info + ": ";
-                let path = props.isDirectory ? entry.path : entry.path.substring(0, entry.path.lastIndexOf(props.name) -1);
-                let item = {
-                    info: info,
-                    link: entry.path,
-                    path: path,
-                    name: props.name.length > 25 ? props.name.substring(0,22) + '...' : props.name,
-                    hasThumbnail: props.thumbnail.ref != null,
-                    thumbnail: props.thumbnail.ref == null ? null : file.ref.getBase64Thumbnail(),
-                    isDirectory: props.isDirectory,
-                    file : file.ref
-                };
-                return future.complete(item);
-            }).exceptionally(function(throwable) {
-                console.log("error loading timeline entry: " + throwable.getMessage());
-            });
-            return future;
+            let props = file.props;
+            if (props.isHidden) {
+                return;
+            }
+            if (props.isDirectory) {
+                info = info + " the directory";
+            } else {
+                info = info + " the file";
+            }
+            if (entry.sharer != entry.owner) {
+                info = info + " owned by " + entry.owner;
+            }
+            info = info + ": ";
+            let path = props.isDirectory ? entry.path : entry.path.substring(0, entry.path.lastIndexOf(props.name) -1);
+            let item = {
+                info: info,
+                link: entry.path,
+                path: path,
+                name: props.name.length > 25 ? props.name.substring(0,22) + '...' : props.name,
+                hasThumbnail: props.thumbnail.ref != null,
+                thumbnail: props.thumbnail.ref == null ? null : file.getBase64Thumbnail(),
+                isDirectory: props.isDirectory,
+                file : file
+            };
+            return item;
         },
         getFileIconClass: function(file) {
             return this.getFileIcon(file);
@@ -86,21 +76,25 @@ module.exports = {
             that.showSpinner = true;
             var ctx = this.context;
             this.socialFeed.getShared(0, 100, ctx.crypto, ctx.network).thenApply(function(items) {
-                let arr = items.toArray();
-                let numberOfEntries = arr.length;
+                let allEntries = items.toArray();
                 let allTimelineEntries = [];
+                let numberOfEntries = allEntries.length;
                 if (numberOfEntries == 0) {
                     that.data = allTimelineEntries;
                     that.showSpinner = false;
                 } else {
-                    arr.forEach(function(entry,idx){
-                        that.createTimelineEntry(entry).thenApply(function(timelineEntry){
-                            allTimelineEntries.push(timelineEntry);
+                    ctx.getFiles(items).thenApply(function(files) {
+                        let allFiles = files.toArray();
+                        allEntries.forEach(function(entry, idx){
+                            allTimelineEntries.push(that.createTimelineEntry(entry, allFiles[idx]));
                             if (numberOfEntries == idx + 1) {
                                 that.data = allTimelineEntries;
                                 that.showSpinner = false;
                             }
                         });
+                    }).exceptionally(function(throwable) {
+                        that.showMessage(throwable.getMessage());
+                        that.showSpinner = false;
                     });
                 }
             }).exceptionally(function(throwable) {
