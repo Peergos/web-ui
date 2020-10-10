151ce5009ed44215c17fe237a8df331bcbf54099
kevin odwyer <kevodwyer@gmail.com>
2020-10-10 19:08:53 +0100

    add paging

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -3,7 +3,11 @@ module.exports = {
     data: function() {
         return {
             showSpinner: false,
-            data: []
+            data: [],
+            pageEndIndex : 0,
+            pageSize: 20,
+            requestingMoreResults: false,
+            noMoreResults: false
         }
     },
     props: ['context','navigateToAction','viewAction', 'messages', 'getFileIcon', 'socialFeed'],
@@ -12,9 +16,61 @@ module.exports = {
         Vue.nextTick(function() {
             that.init();
         });
-
     },
     methods: {
+        handleScrolling: function() {
+            let that = this;
+            let scrollingDiv = document.getElementById('scroll-area');
+            if (scrollingDiv.offsetHeight + scrollingDiv.scrollTop >= scrollingDiv.scrollHeight) {
+                that.requestMoreResults();
+            }
+        },
+        processItems: function(items) {
+            var that = this;
+            var ctx = this.context;
+            let allTimelineEntries = [];
+            let numberOfEntries = items.length;
+            ctx.getFiles(items).thenApply(function(files) {
+                let allFiles = files.toArray();
+                items.forEach(function(item, idx){
+                    allTimelineEntries.push(that.createTimelineEntry(item, allFiles[idx]));
+                    if (numberOfEntries == idx + 1) {
+                        that.data = that.data.concat(allTimelineEntries);
+                        that.showSpinner = false;
+                        that.requestingMoreResults = false;
+                    }
+                });
+            }).exceptionally(function(throwable) {
+                that.showMessage(throwable.getMessage());
+                that.showSpinner = false;
+                that.requestingMoreResults = false;
+            });
+        },
+        requestMoreResults: function() {
+            let that = this;
+            if (that.noMoreResults || that.requestingMoreResults) {
+                return;
+            }
+            that.showSpinner = true;
+            that.requestingMoreResults = true;
+            let startIndex = Math.max(0, this.pageEndIndex - this.pageSize);
+            this.retrieveResults(startIndex, this.pageEndIndex).thenApply(function(additionalItems) {
+               that.pageEndIndex = that.pageEndIndex - additionalItems.length;
+               let items = additionalItems.reverse();
+               if (items.length == 0) {
+                    that.showSpinner = false;
+                    that.requestingMoreResults = false;
+                    that.noMoreResults = true;
+                    that.data = that.data.concat({isLastEntry: true});
+               } else {
+                    that.processItems(items);
+               }
+            }).exceptionally(function(throwable) {
+                that.showMessage(throwable.getMessage());
+                that.showSpinner = false;
+                that.requestingMoreResults = false;
+            });
+        },
         showMessage: function(title, body) {
             this.messages.push({
                 title: title,
@@ -61,40 +117,90 @@ module.exports = {
                 hasThumbnail: props.thumbnail.ref != null,
                 thumbnail: props.thumbnail.ref == null ? null : file.getBase64Thumbnail(),
                 isDirectory: props.isDirectory,
-                file : file
+                file : file,
+                isLastEntry: false
             };
             return item;
         },
         getFileIconClass: function(file) {
             return this.getFileIcon(file);
         },
-
-	    init: function() {
-            var that = this;
-            that.showSpinner = true;
-            var ctx = this.context;
-            this.socialFeed.getShared(0, 100, ctx.crypto, ctx.network).thenApply(function(items) {
-                let allEntries = items.toArray();
-                let allTimelineEntries = [];
-                let numberOfEntries = allEntries.length;
-                if (numberOfEntries == 0) {
-                    that.data = allTimelineEntries;
-                    that.showSpinner = false;
-                } else {
-                    ctx.getFiles(items).thenApply(function(files) {
-                        let allFiles = files.toArray();
-                        allEntries.forEach(function(entry, idx){
-                            allTimelineEntries.push(that.createTimelineEntry(entry, allFiles[idx]));
-                            if (numberOfEntries == idx + 1) {
-                                that.data = allTimelineEntries;
-                                that.showSpinner = false;
-                            }
-                        });
+	    retrieveUnSeen: function(startIndex, requestSize, results) {
+	        var future = peergos.shared.util.Futures.incomplete();
+	        this.retrieveUnSeenWithFuture(startIndex, requestSize, results, future);
+	        return future;
+        },
+	    retrieveUnSeenWithFuture: function(startIndex, requestSize, results, future) {
+	        if (! this.socialFeed.hasUnseen() ) {
+	            future.complete(results);
+	        } else {
+                var ctx = this.context;
+                let that = this;
+                this.socialFeed.getShared(startIndex, startIndex + requestSize, ctx.crypto, ctx.network).thenApply(function(items) {
+                    let allEntries = items.toArray();
+                    let newIndex = startIndex + allEntries.length;
+                    that.socialFeed.setLastSeenIndex(newIndex).thenApply(function(res) {
+                        that.retrieveUnSeenWithFuture(newIndex, requestSize, results.concat(allEntries), future);
                     }).exceptionally(function(throwable) {
                         that.showMessage(throwable.getMessage());
                         that.showSpinner = false;
                     });
-                }
+                }).exceptionally(function(throwable) {
+                    that.showMessage(throwable.getMessage());
+                    that.showSpinner = false;
+                });
+            }
+            return future;
+        },
+	    retrieveResults: function(startIndex, endIndex) {
+	        var future = peergos.shared.util.Futures.incomplete();
+	        if(startIndex < 0 || startIndex >= endIndex) {
+    	        future.complete([]);
+	            return future;
+	        }
+            var ctx = this.context;
+            this.socialFeed.getShared(startIndex, endIndex, ctx.crypto, ctx.network).thenApply(function(items) {
+                future.complete(items.toArray());
+            }).exceptionally(function(throwable) {
+                that.showMessage(throwable.getMessage());
+                that.showSpinner = false;
+            });
+            return future;
+	    },
+	    init: function() {
+            var that = this;
+            that.showSpinner = true;
+            var ctx = this.context;
+            this.pageEndIndex = this.socialFeed.getLastSeenIndex();
+            this.retrieveUnSeen(this.pageEndIndex, this.pageSize, []).thenApply(function(unseenItems) {
+                let startIndex = Math.max(0, that.pageEndIndex - that.pageSize);
+                that.retrieveResults(startIndex, that.pageEndIndex, []).thenApply(function(additionalItems) {
+                    that.pageEndIndex = that.pageEndIndex - additionalItems.length;
+                    let allTimelineEntries = [];
+                    let items = unseenItems.reverse().concat(additionalItems.reverse());
+                    let numberOfEntries = items.length;
+                    if (numberOfEntries == 0) {
+                        that.data = allTimelineEntries;
+                        that.showSpinner = false;
+                    } else {
+                        ctx.getFiles(items).thenApply(function(files) {
+                            let allFiles = files.toArray();
+                            items.forEach(function(item, idx){
+                                allTimelineEntries.push(that.createTimelineEntry(item, allFiles[idx]));
+                                if (numberOfEntries == idx + 1) {
+                                    that.data = allTimelineEntries;
+                                    that.showSpinner = false;
+                                }
+                            });
+                        }).exceptionally(function(throwable) {
+                            that.showMessage(throwable.getMessage());
+                            that.showSpinner = false;
+                        });
+                    }
+                }).exceptionally(function(throwable) {
+                    that.showMessage(throwable.getMessage());
+                    that.showSpinner = false;
+                });
             }).exceptionally(function(throwable) {
                 that.showMessage(throwable.getMessage());
                 that.showSpinner = false;
