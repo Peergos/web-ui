2eba7540ee113746d2c05f506ce6ae818a3019c9
ian <ianopolous@protonmail.com>
2020-10-13 15:00:26 +0100

    update to generic list method

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -30,7 +30,7 @@ module.exports = {
             var ctx = this.context;
             let allTimelineEntries = [];
             let numberOfEntries = items.length;
-            peergos.client.JSUtil.getFiles(ctx, items).thenApply(function(files) {
+            ctx.getFiles(peergos.client.JsUtil.asList(items)).thenApply(function(files) {
                 let allFiles = files.toArray();
                 items.forEach(function(item, idx){
                     allTimelineEntries.push(that.createTimelineEntry(item, allFiles[idx]));
@@ -185,7 +185,7 @@ module.exports = {
                         that.data = allTimelineEntries;
                         that.showSpinner = false;
                     } else {
-                        peergos.client.JSUtil.getFiles(ctx, items).thenApply(function(files) {
+                        ctx.getFiles(peergos.client.JsUtil.asList(items)).thenApply(function(files) {
                             let allFiles = files.toArray();
                             items.forEach(function(item, idx){
                                 allTimelineEntries.push(that.createTimelineEntry(item, allFiles[idx]));
