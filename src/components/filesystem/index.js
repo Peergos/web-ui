1bc741fcc865f1725b64dea39c0cb25901460a19
kevin odwyer <kevodwyer@gmail.com>
2021-05-25 12:08:58 +0100

    make file system navigatable

--- src/components/filesystem/index.js
+++ src/components/filesystem/index.js
@@ -113,6 +113,7 @@ module.exports = {
         this.showTour = this.newsignup;
         this.init();
         window.onhashchange = this.onUrlChange;
+        this.buildTabNavigation();
     },
     watch: {
         // manually encode currentDir dependencies to get around infinite dependency chain issues with async-computed methods
@@ -227,6 +228,91 @@ module.exports = {
         }
         this.showPendingServerMessages();
 	},
+	clearTabNavigation: function() {
+	    let that = this;
+	    Vue.nextTick(function() {
+            let gridItems = document.getElementsByClassName('grid-item');
+            let appGridItems = document.getElementsByClassName('app-grid-item');
+            let toolbarItems = document.getElementsByClassName('toolbar-item');
+            for(var i=0; i < gridItems.length; i++) {
+                gridItems[i].removeAttribute("tabindex");
+            }
+            for(var j=0; j < appGridItems.length; j++) {
+                appGridItems[j].removeAttribute("tabindex");
+            }
+            for(var k=0; k < toolbarItems.length; k++) {
+                toolbarItems[k].removeAttribute("tabindex");
+            }
+	    });
+    },
+	buildTabNavigation: function() {
+	    let that = this;
+	    Vue.nextTick(function() {
+            let gridItems = document.getElementsByClassName('grid-item');
+            let appGridItems = document.getElementsByClassName('app-grid-item');
+            let uploadItems = document.getElementsByClassName('upload-item');
+            let toolbarItems = document.getElementsByClassName('toolbar-item');
+            let settingsItems = document.getElementsByClassName('settings-item');
+
+            if (that.showAppgrid) {
+                for(var j=0; j < appGridItems.length; j++) {
+                    appGridItems[j].setAttribute("tabindex", 0);
+                }
+            } else {
+                if (that.showUploadMenu) {
+                    that.showSettingsMenu = false;
+                    for(var i=0; i < gridItems.length; i++) {
+                        gridItems[i].removeAttribute("tabindex");
+                    }
+                    for(var k=0; k < uploadItems.length; k++) {
+                        uploadItems[k].setAttribute("tabindex", 0);
+                    }
+                    for(var l=0; l < toolbarItems.length; l++) {
+                        toolbarItems[l].removeAttribute("tabindex");
+                    }
+                    document.getElementById("uploadButton").setAttribute("tabindex", 0);
+                } else if (that.showSettingsMenu) {
+                    that.showUploadMenu = false;
+                    for(var i=0; i < gridItems.length; i++) {
+                        gridItems[i].removeAttribute("tabindex");
+                    }
+                    for(var m=0; m < settingsItems.length; m++) {
+                        settingsItems[m].setAttribute("tabindex", 0);
+                    }
+                    for(var l=0; l < toolbarItems.length; l++) {
+                        toolbarItems[l].removeAttribute("tabindex");
+                    }
+                    document.getElementById("settings-menu").setAttribute("tabindex", 0);
+                } else if (that.viewMenu) {
+                    that.showSettingsMenu = false;
+                    that.showUploadMenu = false;
+                    for(var i=0; i < gridItems.length; i++) {
+                        gridItems[i].removeAttribute("tabindex");
+                    }
+                    for(var l=0; l < toolbarItems.length; l++) {
+                        toolbarItems[l].removeAttribute("tabindex");
+                    }
+                } else {
+                    for(var i=0; i < gridItems.length; i++) {
+                        gridItems[i].setAttribute("tabindex", 0);
+                    }
+                    for(var l=0; l < toolbarItems.length; l++) {
+                        toolbarItems[l].setAttribute("tabindex", 0);
+                    }
+                }
+            }
+            if (!that.showUploadMenu) {
+                for(var k=0; k < uploadItems.length; k++) {
+                    uploadItems[k].removeAttribute("tabindex");
+                }
+            }
+            if (!that.showSettingsMenu) {
+                for(var m=0; m < settingsItems.length; m++) {
+                    settingsItems[m].removeAttribute("tabindex");
+                }
+            }
+        });
+	},
 	showPendingServerMessages: function() {
         let context = this.getContext();
         let that = this;
@@ -260,6 +346,7 @@ module.exports = {
             this.showAppgrid = false;
             this.view="files";
             this.path = data.path;
+            this.buildTabNavigation();
         },
     processPending: function() {
         for (var i=0; i < this.onUpdateCompletion.length; i++) {
@@ -441,6 +528,10 @@ module.exports = {
         this.updateHistory("search", this.getPath(), "");
         this.closeMenu();
     },
+    closeSearch: function() {
+        this.showSearch = false;
+        this.buildTabNavigation();
+    },
 	openAppFromFolder: function() {
 	    let path = this.getPath();
 	    let pathItems = path.split('/').filter(n => n.length > 0);
@@ -483,6 +574,7 @@ module.exports = {
                     that.files = arr.filter(function(f){
                         return !f.getFileProperties().isHidden;
                     });
+                    that.buildTabNavigation();
                     if(selectedFilename != null) {
                         that.selectedFiles = that.files.filter(f => f.getName() == selectedFilename);
                         that.gallery();
@@ -618,7 +710,10 @@ module.exports = {
             this.warning_consumer_func = deleteFn;
             this.showWarning = true;
         },
-
+        closeWarning: function() {
+            this.showWarning = false;
+            this.buildTabNavigation();
+        },
         confirmDownload: function(file, downloadFn) {
             var size = this.getFileSize(file.getFileProperties());
             if (this.supportsStreaming() || size < 50*1024*1024)
@@ -1052,6 +1147,7 @@ module.exports = {
         },
         toggleUserMenu: function() {
             this.showSettingsMenu = !this.showSettingsMenu;
+            this.buildTabNavigation();
         },
 
         toggleFeedbackForm: function() { 
@@ -1200,18 +1296,25 @@ module.exports = {
 
         toggleUploadMenu: function() {
             this.showUploadMenu = !this.showUploadMenu;
+            this.buildTabNavigation();
         },
 
         showChangePassword: function() {
             this.toggleUserMenu();
             this.showPassword = true;
         },
-
+        closeChangePassword: function() {
+            this.showPassword = false;
+            this.buildTabNavigation();
+        },
         showViewAccount: function() {
             this.toggleUserMenu();
             this.showAccount = true;
         },
-
+        closeViewAccount: function() {
+            this.showAccount = false;
+            this.buildTabNavigation();
+        },
         showProfile: function(showEditForm) {
             if(! showEditForm) {
                 this.closeMenu();
@@ -1255,14 +1358,19 @@ module.exports = {
         },
 
         showRequestStorage: function() {
-	    var that = this;
-	    this.context.getPaymentProperties(false).thenApply(function(paymentProps) {
-		if (paymentProps.isPaid()) {
-		    that.paymentProperties = paymentProps;
-		    that.showBuySpace = true;
-		} else
-		    that.showRequestSpace = true;
-	    });
+            var that = this;
+            this.toggleUserMenu();
+            this.context.getPaymentProperties(false).thenApply(function(paymentProps) {
+            if (paymentProps.isPaid()) {
+                that.paymentProperties = paymentProps;
+                that.showBuySpace = true;
+            } else
+                that.showRequestSpace = true;
+            });
+        },
+        closeRequestSpace: function() {
+            this.showRequestSpace = false
+            this.buildTabNavigation();
         },
         showTodoBoard: function() {
             let that = this;
@@ -1373,16 +1481,20 @@ module.exports = {
         },
 
         showAdminPanel: function(name) {
-	    const context = this.getContext()
-	    if (context == null)
-		return;
-	    const that = this;
-	    context.getAndDecodePendingSpaceRequests().thenApply(reqs => {
-		that.admindata.pending = reqs.toArray([]);
-		that.showAdmin = true;
-	    });
+            this.toggleUserMenu();
+            const context = this.getContext()
+            if (context == null)
+                return;
+            const that = this;
+            context.getAndDecodePendingSpaceRequests().thenApply(reqs => {
+                that.admindata.pending = reqs.toArray([]);
+                that.showAdmin = true;
+            });
         },
-
+        closeAdmin: function() {
+            this.showAdmin = false;
+            this.buildTabNavigation();
+        },
         showSocialView: function(name) {
             this.showSocial = true;
             this.externalChange++;
@@ -1422,7 +1534,7 @@ module.exports = {
                 op: "copy",
                 path: this.getPath()
             };
-            this.closeMenu();
+            this.closeMenu(true);
         },
 
         cut: function() {
@@ -1436,7 +1548,7 @@ module.exports = {
                 op: "cut",
                 path: this.getPath()
             };
-            this.closeMenu();
+            this.closeMenu(true);
         },
 
         paste: function() {
@@ -1593,7 +1705,10 @@ module.exports = {
             this.allowCreateSecretLink = true;
             this.showShare = true;
         },
-
+        closeShare: function() {
+            this.showShare = false;
+            this.buildTabNavigation();
+        },
         setSortBy: function(prop) {
             if (this.sortBy == prop)
                 this.normalSortOrder = !this.normalSortOrder;
@@ -1651,7 +1766,7 @@ module.exports = {
         downloadAll: function() {
             if (this.selectedFiles.length == 0)
                 return;
-            this.closeMenu();
+            this.closeMenu(true);
             for (var i=0; i < this.selectedFiles.length; i++) {
                 var file = this.selectedFiles[i];
                 this.navigateOrDownload(file);
@@ -1741,23 +1856,24 @@ module.exports = {
 	navigateOrDownload: function(file) {
             if (this.showSpinner) // disable user input whilst refreshing
                 return;
-            this.closeMenu();
-            if (file.isDirectory())
+            this.buildTabNavigation();
+            if (file.isDirectory()) {
                 this.navigateToSubdir(file.getFileProperties().name);
-            else {
-		var that = this;
-		this.confirmDownload(file, () => {that.downloadFile(file);});
-	    }
+            } else {
+		        var that = this;
+		        this.confirmDownload(file, () => {that.downloadFile(file);});
+	        }
         },
 
         navigateOrMenu: function(event, file) {
             if (this.showSpinner) // disable user input whilst refreshing
                 return;
             this.closeMenu();
-            if (file.isDirectory())
+            if (file.isDirectory()) {
                 this.navigateToSubdir(file.getFileProperties().name);
-            else
+            } else {
                 this.openMenu(event, file);
+            }
         },
 
         navigateToSubdir: function(name) {
@@ -1910,6 +2026,7 @@ module.exports = {
                     this.selectedFiles = [file];
                 }
                 this.viewMenu = true;
+                this.buildTabNavigation();
                 Vue.nextTick(function() {
                     var menu = document.getElementById("right-click-menu-profile");
                     if (menu != null)
@@ -1937,7 +2054,7 @@ module.exports = {
             }
         },
         createTextFile: function() {
-            this.closeMenu();
+            this.closeMenu(true);
             this.prompt_placeholder='File name';
             this.prompt_message='Enter a file name';
             this.prompt_value='';
@@ -2018,7 +2135,10 @@ module.exports = {
             };
             this.showPrompt =  true;
         },
-
+        closePrompt: function() {
+            this.showPrompt = false;
+            this.buildTabNavigation();
+        },
         deleteFiles: function() {
             var selectedCount = this.selectedFiles.length;
             if (selectedCount == 0)
@@ -2027,10 +2147,13 @@ module.exports = {
 
             for (var i=0; i < selectedCount; i++) {
                 var file = this.selectedFiles[i];
-		var that = this;
-		var parent = this.currentDir;
-		var context = this.getContext();
-                this.confirmDelete(file, () => that.deleteOne(file, parent, context));
+                var that = this;
+                var parent = this.currentDir;
+                var context = this.getContext();
+                this.confirmDelete(file, () => {
+                    that.deleteOne(file, parent, context);
+                    that.buildTabNavigation();
+                });
             }
         },
 
@@ -2087,9 +2210,14 @@ module.exports = {
                 return false;
             return this.sharedWithState.isShared(file.getFileProperties().name);
         },
-        closeMenu: function() {
+        closeMenu: function(ignoreClearTabNavigation) {
             this.viewMenu = false;
             this.ignoreEvent = false;
+            if (ignoreClearTabNavigation) {
+                this.buildTabNavigation();
+            } else {
+                this.clearTabNavigation();
+            }
         },
         toggleNav : function() {
             if (this.showAppgrid)
@@ -2097,6 +2225,7 @@ module.exports = {
             else
                 this.view = "appgrid"
             this.showAppgrid = ! this.showAppgrid;
+            this.buildTabNavigation();
         },
         formatDateTime: function(dateTime) {
             let date = new Date(dateTime.toString() + "+00:00");//adding UTC TZ in ISO_OFFSET_DATE_TIME ie 2021-12-03T10:25:30+00:00
