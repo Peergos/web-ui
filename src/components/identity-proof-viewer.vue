07e56615729a1f8c836569ff69e9906b4e9a2fab
ian <ianopolous@protonmail.com>
2021-09-04 21:19:54 +0100

    Implement a viewer for identity proofs
    Fix public links to files (only browse to the file's parent dir)
    Make sure open request propagates from public link to secret link,
    and opens identity proofs.

---
+++ src/components/identity-proof-viewer.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="modal-mask" @click="close">
+        <div class="modal-container" @click.stop style="height:95%;width:95%;overflow-y:auto;max-width:800px;">
+            <spinner v-if="showSpinner"></spinner>
+<div>
+    <h2>Identity Proof</h2>
+    <div v-if="proof != null">
+        User {{ proof.claim.peergosUsername }} on Peergos owns account {{ proof.claim.alternateUsername }} on {{ proof.claim.alternateService }}.
+        <br/>
+        Signature: {{ proof.encodedSignature() }}
+        <br/>
+        <label>Proof: </label><a v-if="proof.hasUrl()" v-bind:href="proof.alternateUrl.get()" target="_blank">{{ proof.alternateUrl.get() }}</a>
+    </div>
+</div>
+</div>
+</div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            proof: null
+        };
+    },
+    props: ["file", "context"],
+    created: function() {
+        this.updateCurrentFileData();
+    },
+    methods: {
+        downloadCurrentFile: function() {
+            this.downloadFile(this.file);
+        },
+        close: function() {
+            this.$emit("hide-identity-proof");
+        },
+
+        updateCurrentFileData: function() {
+            if (this.file == null)
+                return;
+            if (this.file.isDirectory())
+                return;
+            var props = this.file.getFileProperties();
+            var that = this;
+            this.showSpinner = true;
+            this.file.getInputStream(this.context.network, this.context.crypto, 
+                props.sizeHigh(), props.sizeLow(), 
+                function(read) {})
+                .thenCompose(function(startReader) {
+                    return startReader
+                        .seekJS(0, 0).thenCompose(function(reader) {
+
+                            var sizeToRead = Math.min(5*1024*1024, props.sizeLow());
+                            var data = convertToByteArray(new Int8Array(sizeToRead));
+                            data.length = sizeToRead;
+                            return reader.readIntoArray(data, 0, data.length)
+                                .thenApply(function(read){
+                                    that.proof = peergos.shared.util.Serialize.parse(data, c => peergos.shared.user.AlternateIdentityProof.fromCbor(c));
+                                    that.showSpinner = false;
+                                    console.log("Finished retrieving file section of size " + data.length);
+                                });
+                        });
+                });
+        },
+    }
+};
+</script>
