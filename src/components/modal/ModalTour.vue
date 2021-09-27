f9275e9020c9bc489f1bafd227f8ea34f180b524
Manuel Ehrenfeld <manuel@non-linear.com>
2021-09-27 16:02:12 +0200

    centered modal, tour slider

---
+++ src/components/modal/ModalTour.vue
@@ -0,0 +1,96 @@
+<template>
+	<AppModal position="center">
+		<template #header>
+			<h2>Tour</h2>
+		</template>
+		<template #body>
+			<Slider :slides="slides"/>
+		</template>
+		<template #footer>
+		</template>
+	</AppModal>
+</template>
+
+<script>
+const Slider = require("../slider/Slider.vue");
+
+module.exports = {
+components: {
+	Slider,
+},
+data() {
+	return {
+	    slides: [
+			{
+				title: "Welcome to Peergos!",
+				description: "Learn some of the cool things you can do with Peergos!"
+			},
+			{
+				title: "Safe storage",
+				description: "Store your files and arrange them in folders.",
+				image: "images/tour/upload.png"
+			},
+			{
+				title: "Find your friends",
+				description: "Add friends with a secure follow request.",
+				image: "images/tour/social.png"
+			},
+			{
+				title: "Secure sharing",
+				description: "Securely share files or folders with friends.",
+				image: "images/tour/share.png"
+			},
+			{
+				title: "Secret links",
+				description: "Create a secret link to share with anyone.",
+				image: "images/tour/secret.png"
+			},
+			{
+				title: "Photo gallery",
+				description: "View slideshows of your photos.",
+				image: "images/tour/images.png"
+			},
+			{
+				title: "Document editing",
+				description: "View and edit your text documents.",
+				image: "images/tour/text.png"
+			},
+			{
+				title: "Music collection",
+				description: "Play your music collection.",
+				image: "images/tour/music.png"
+			},
+			{
+				title: "Videos and movies",
+				description: "Watch your videos and movies from any device.",
+				image: "images/tour/video.png"
+			},
+			{
+				title: "PDF viewer",
+				description: "Safely view PDF documents.",
+				image: "images/tour/pdf.png"
+			},
+			{
+				title: "Planning board",
+				description: "Organise and plan things with our planning boards.",
+				image: "images/tour/kanban.png"
+			},
+			{
+				title: "Calendar",
+				description: "Manage your schedule - share events or entire calendars.",
+				image: "images/tour/calendar.png"
+			},
+			{
+				title: "Social Feed",
+				description: "Share posts and comments with your friends and followers.",
+				image: "images/tour/social-feed.png"
+			},
+		]
+	}
+},
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/slider/Slider.vue
@@ -0,0 +1,149 @@
+<template>
+	<div class="app-slider">
+		<transition-group
+			tag="div"
+			:name="transitionName"
+			class="slider__container"
+		>
+			<div
+				v-if="show"
+				:key="current"
+				class="slide"
+			>
+				<img
+					v-if="slides[current].image"
+					:src="slides[current].image"
+					:alt="slides[current].title"
+					class="slide__image"
+				/>
+				<h2 class="slide__title">
+					{{ slides[current].title }}
+				</h2>
+				<p class="slide__description">
+					{{ slides[current].description }}
+				</p>
+			</div>
+		</transition-group>
+		<span class="slider__pagination">
+			{{ current }}/ {{ slidesLength }}
+		</span>
+		<!-- <AppButton
+			class="slider__button button--prev"
+			aria-label="Previous slide"
+			@click.native="slide(-1)"
+			icon="arrow-right"
+		>
+		</AppButton> -->
+		<AppButton
+			class="slider__button button--next"
+			aria-label="Next slide"
+			@click.native="slide(1)"
+			icon="arrow-right"
+			accent
+		>
+			next
+		</AppButton>
+	</div>
+</template>
+
+<script>
+module.exports = {
+	props: {
+		slides: {
+			type: Array,
+			default: () => [],
+		},
+	},
+	data() {
+		return {
+			current: 0,
+			direction: 1,
+			transitionName: "fade",
+			show: false,
+		};
+	},
+	computed: {
+		slidesLength() {
+			return this.slides.length;
+		}
+	},
+	methods: {
+		slide(dir) {
+			this.direction = dir;
+			// dir === 1
+			// 	? (this.transitionName = "slide-next")
+			// 	: (this.transitionName = "slide-prev");
+			this.current = (this.current + (dir % this.slidesLength) + this.slidesLength) % this.slidesLength;
+		},
+	},
+	mounted() {
+		this.show = true;
+	},
+};
+</script>
+
+<style>
+.app-slider {
+	position: relative;
+}
+.slider__container {
+	min-width: 500px;
+	min-height: 500px;
+}
+.app-slider .slide {
+	position: absolute;
+	top:0;
+	left: 0;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+
+}
+
+.app-slider .slide__image {
+	width: 100%;
+	height: auto;
+}
+
+
+.slider__button {
+	transition: transform 0.3s ease-in-out;
+	user-select: none;
+}
+
+.slider__button.button--next {
+	position: absolute;
+	right:0;
+}
+
+.slider__button--prev svg{
+	transform: rotate(180deg);
+}
+
+
+.slider__pagination{}
+
+/* Next Slide */
+.slide-next-enter-active,
+.slide-next-leave-active {
+	transition: transform 0.5s ease-in-out;
+}
+.slide-next-enter {
+	transform: translate(100%);
+}
+.slide-next-leave-to {
+	transform: translate(-100%);
+}
+
+/* Prev slide */
+.slide-prev-enter-active,
+.slide-prev-leave-active {
+	transition: transform 0.5s ease-in-out;
+}
+.slide-prev-enter {
+	transform: translate(-100%);
+}
+.slide-prev-leave-to {
+	transform: translate(100%);
+}
+</style>
