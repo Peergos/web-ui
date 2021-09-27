284acabcfeb96a99c0cdf9061a2bfcbc4c84fd1c
ian <ianopolous@protonmail.com>
2021-09-27 22:42:44 +0100

    Update tour images

--- src/components/modal/ModalTour.vue
+++ src/components/modal/ModalTour.vue
@@ -1,7 +1,7 @@
 <template>
 	<AppModal position="center">
 		<template #header>
-			<h2>Tour</h2>
+			<h2>Welcome to Peergos!</h2>
 		</template>
 		<template #body>
 			<Slider :slides="slides"/>
@@ -22,48 +22,34 @@ data() {
 	return {
 	    slides: [
 			{
-				title: "Welcome to Peergos!",
-				description: "Learn some of the cool things you can do with Peergos!"
-			},
-			{
-				title: "Safe storage",
+				title: "Encrypted storage",
 				description: "Store your files and arrange them in folders.",
-				image: "images/tour/upload.png"
-			},
-			{
-				title: "Find your friends",
-				description: "Add friends with a secure follow request.",
-				image: "images/tour/social.png"
+				image: "images/tour/drive.png"
 			},
 			{
 				title: "Secure sharing",
-				description: "Securely share files or folders with friends.",
+				description: "Securely share with friends on Peergos, or with anyone via a secret link.",
 				image: "images/tour/share.png"
-			},
-			{
-				title: "Secret links",
-				description: "Create a secret link to share with anyone.",
-				image: "images/tour/secret.png"
 			},
 			{
 				title: "Photo gallery",
 				description: "View slideshows of your photos.",
-				image: "images/tour/images.png"
+				image: "images/tour/images.jpg"
 			},
 			{
 				title: "Document editing",
 				description: "View and edit your text documents.",
-				image: "images/tour/text.png"
+				image: "images/tour/markdown.png"
 			},
 			{
 				title: "Music collection",
 				description: "Play your music collection.",
-				image: "images/tour/music.png"
+				image: "images/tour/music.jpg"
 			},
 			{
 				title: "Videos and movies",
 				description: "Watch your videos and movies from any device.",
-				image: "images/tour/video.png"
+				image: "images/tour/video.jpg"
 			},
 			{
 				title: "PDF viewer",
@@ -71,9 +57,9 @@ data() {
 				image: "images/tour/pdf.png"
 			},
 			{
-				title: "Planning board",
-				description: "Organise and plan things with our planning boards.",
-				image: "images/tour/kanban.png"
+				title: "Tasks board",
+				description: "Organise and plan things with our task boards.",
+				image: "images/tour/tasks.png"
 			},
 			{
 				title: "Calendar",
