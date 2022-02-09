78f46650ba03ff37481a4b7296bb0dac0d084f5c
ian <ianopolous@protonmail.com>
2022-02-09 20:51:09 +0000

    make native image install script cross platform

---
+++ native-build/InstallNativeImage.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.net.*;
+import java.util.zip.*;
+
+public class InstallNativeImage {
+
+    public static void main(String[] a) throws Exception {
+        String VERSION = "22.0.0.2";
+        
+        String OS = canonicaliseOS(System.getProperty("os.name").toLowerCase());
+        String OS_ARCH = getOsArch();
+        System.out.println("OS-ARCH: " + OS_ARCH);
+        String ext = OS.equals("windows") ? ".zip" : ".tar.gz";
+        String filename = "graalvm-ce-java11-"+OS_ARCH+"-"+VERSION+ext;
+        String url = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-" +VERSION +"/" + filename;
+
+        // Download graalVM
+        if (! new File(filename).exists())
+            download(url, new File(filename));
+        String tar = OS.equals("windows") ? "unzip" : "tar -zxvf";
+
+        // unzip GraalVM
+        runCommand(tar + " " + filename);
+
+        // install native-image
+        runCommand("./graalvm-ce-java11-" + VERSION + "/bin/gu install native-image");
+
+        if (new File("graalvm-ce-java11-"+VERSION + "/bin/native-image").exists())
+            System.out.println("native-image installed");
+        else
+            System.out.println("native-image not installed...");
+    }
+
+    public static void download(String url, File output) throws Exception {
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setDoInput(true);
+
+        String contentEncoding = conn.getContentEncoding();
+        boolean isGzipped = "gzip".equals(contentEncoding);
+        DataInputStream din = new DataInputStream(isGzipped ? new GZIPInputStream(conn.getInputStream()) : conn.getInputStream());
+        byte[] buf = new byte[4*1024];
+        OutputStream fout = new BufferedOutputStream(new FileOutputStream(output));
+        int nRead;
+        while ((nRead = din.read(buf, 0, buf.length)) != -1 )
+            fout.write(buf, 0, nRead);
+        din.close();
+        fout.flush();
+        fout.close();
+    }
+
+    public static int runCommand(String command) throws Exception {
+        System.out.println(command);
+        ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
+        pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        return pb.start().waitFor();
+    }
+
+    private static String getOsArch() {
+        String os = canonicaliseOS(System.getProperty("os.name").toLowerCase());
+        String arch = canonicaliseArchitecture(System.getProperty("os.arch"));
+
+        return os + "-" + arch;
+    }
+
+    private static String canonicaliseArchitecture(String arch) {
+        if (arch.startsWith("arm64"))
+            return "arm64";
+        if (arch.startsWith("arm"))
+            return "arm";
+        if (arch.startsWith("x86_64"))
+            return "amd64";
+        if (arch.startsWith("x86"))
+            return "386";
+        return arch;
+    }
+
+    private static String canonicaliseOS(String os) {
+        if (os.startsWith("mac"))
+            return "darwin";
+        if (os.startsWith("windows"))
+            return "windows";
+        return os;
+    }
+}
