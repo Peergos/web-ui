7b34d0f257f6bdbeca2c01581719775f34006b02
kevin odwyer <kevodwyer@gmail.com>
2022-11-02 14:50:43 +0000

    undo urldecode change

--- src/mixins/uridecoder/index.js
+++ src/mixins/uridecoder/index.js
@@ -1,21 +1,17 @@
 module.exports = {
     methods: {
         uriDecode: function(urlencoded) {
-            if (urlencoded != null) {
-                let jsErrorBody = urlencoded.split("\\+").join("%20")
-                    .split("\\%21").join("!")
-                    .split("\\%27").join("'")
-                    .split("\\%28").join("(")
-                    .split("\\%29").join(")")
-                    .split("\\%7E").join("~")
-                    .split("+").join("%20");
+            let jsErrorBody = urlencoded.split("\\+").join("%20")
+                .split("\\%21").join("!")
+                .split("\\%27").join("'")
+                .split("\\%28").join("(")
+                .split("\\%29").join(")")
+                .split("\\%7E").join("~")
+                .split("+").join("%20");
 
-                let str = decodeURIComponent(jsErrorBody);
-                let token = 'java.lang.JsException: ';
-                return str.startsWith(token) ? str.substring(token.length) : str;
-            } else {
-                return "Unknown Error";
-            }
+            let str = decodeURIComponent(jsErrorBody);
+            let token = 'java.lang.JsException: ';
+            return str.startsWith(token) ? str.substring(token.length) : str;
         }
     }
 }
