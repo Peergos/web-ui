47dc64fcaa26337159fc7b9d9b2694604a5f755b
ian <ianopolous@protonmail.com>
2024-03-15 14:11:45 +0000

    integrate simplified chinese translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
 const enGB = require("en-GB.js")
+const zhCN = require("zh-CN.js")
 
-const supported = ["en-GB"]
+const supported = ["en-GB", "zh-CN"]
 module.exports = {
 
     methods: {
@@ -11,6 +12,11 @@ module.exports = {
                 locale = "en-GB";
             if (locale == "en-GB") {
                 const res = enGB[label];
+                if (res != null)
+                    return res;
+            }
+            if (locale == "zh-CN") {
+                const res = zhCN[label];
                 if (res != null)
                     return res;
             }
