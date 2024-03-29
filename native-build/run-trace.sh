526476855443e1435aec656ab39d177f6fb8261a
ian <ianopolous@protonmail.com>
2024-03-29 23:37:07 +0000

    typo

--- native-build/run-trace.sh
+++ native-build/run-trace.sh
@@ -4,4 +4,4 @@ export PATH=/home/ian/web-ui/native-build/graalvm-community-openjdk-22+36.1/bin:
 
 java -agentlib:native-image-agent=config-output-dir=META-INF/native-image -jar Peergos.jar daemon -PEERGOS_PATH /home/ian/web-ui/native-build/.peergos -log-to-console true
 
-# modify generated resource-config.json to include webroot/**
+# modify generated resource-config.json to include webroot/.*
