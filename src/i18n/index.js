699680184e02cb28453216b13a973fc3248c97bf
ian <ianopolous@protonmail.com>
2024-12-12 09:27:59 +0000

    Fix italian translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,21 +2,21 @@ const enGB = require("en-GB.js")
 const zhCN = require("zh-CN.js")
 const itIT = require("it-IT.js")
 
-const supported = ["en-GB", "it-IT", "zh-CN"]
+const supported = ["en-GB", "it", "it-CH", "it-IT", "it-SM", "it-VA", "zh-CN"]
 module.exports = {
 
     methods: {
         translate(label, locale) {
             if (locale == null)
                 locale = navigator.language;
-            if (!supported.includes(locale))
+            if (! supported.includes(locale))
                 locale = "en-GB";
             if (locale == "en-GB") {
                 const res = enGB[label];
                 if (res != null)
                     return res;
             }
-            if (locale == "it-IT") {
+            if (locale== "it" || locale.startsWith("it-")) {
                 const res = itIT[label];
                 if (res != null)
                     return res;
