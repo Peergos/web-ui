5e90999492a69e3b8d444f37f580954f067a7b8d
ian <ianopolous@protonmail.com>
2024-12-12 11:58:58 +0000

    Add French

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,26 +1,42 @@
 const enGB = require("en-GB.js")
+const fr = require("fr.js")
+const it = require("it.js")
 const zhCN = require("zh-CN.js")
-const itIT = require("it-IT.js")
 
-const supported = ["en-GB", "it", "it-CH", "it-IT", "it-SM", "it-VA", "zh-CN"]
+const supported = ["en-GB", "zh-CN"]
+const supported_prefixes = ["it", "fr"]
 module.exports = {
 
     methods: {
         translate(label, locale) {
             if (locale == null)
                 locale = navigator.language;
-            if (! supported.includes(locale))
-                locale = "en-GB";
+            if (! supported.includes(locale)) {
+                isSupportedPrefix = false;
+                for (var i=0; i < supported_prefixes.length; i++)
+                    if (locale == supported_prefixes[i] || locale.startsWith(supported_prefixes[i]+"-")) {
+                        isSupportedPrefix = true;
+                        locale = supported_prefixes[i];
+                    }
+                if (! isSupportedPrefix)
+                    locale = "en-GB";
+            }
             if (locale == "en-GB") {
                 const res = enGB[label];
                 if (res != null)
                     return res;
             }
-            if (locale== "it" || locale.startsWith("it-")) {
-                const res = itIT[label];
+            if (locale== "fr") {
+                const res = fr[label];
                 if (res != null)
                     return res;
             }
+            if (locale== "it") {
+                const res = it[label];
+                if (res != null)
+                    return res;
+            }
+            
             if (locale == "zh-CN") {
                 const res = zhCN[label];
                 if (res != null)
