06b5a363b395723ce50e7420b62a7171559da3f6
ian <ianopolous@protonmail.com>
2025-02-06 08:02:33 +0000

    mac keychain path

--- packager/PackagePeergos.java
+++ packager/PackagePeergos.java
@@ -46,6 +46,7 @@ public class PackagePeergos {
                        "--mac-package-identifier", "org.peergos",
                        "--app-version", VERSION,
                        "--mac-sign",
+                       "--mac-signing-keychain", System.getEnv("RUNNER_TEMP") + "/app-signing.keychain-db",
                        "--mac-package-name", "Peergos",
                        "--mac-signing-key-user-name", "Peergos LTD"
                        );
