f69a0e636e177300953d18a4458477565ad40f7d
ian <ianopolous@protonmail.com>
2025-02-06 13:36:13 +0000

    list mac signers

--- packager/PackagePeergos.java
+++ packager/PackagePeergos.java
@@ -32,7 +32,8 @@ public class PackagePeergos {
                        "--resource-dir", "deb-resources",
                        "--app-version", VERSION,
                        "--win-console");
-        else if (isMac)
+        else if (isMac) {
+            runCommand("security", "find-identity", "-v", "-p", "codesigning", System.getenv("RUNNER_TEMP") + "/app-signing.keychain-db");
             runCommand("jpackage", "-i", "../server", "-n", "peergos",
                        "--main-class", "peergos.server.Main", "--main-jar",
                        "Peergos.jar", "--vendor", "Peergos Ltd.",
@@ -50,7 +51,7 @@ public class PackagePeergos {
                        "--mac-package-name", "Peergos",
                        "--mac-signing-key-user-name", "Peergos LTD (XUVT52ZN3F)"
                        );
-        else
+        } else
             runCommand("jpackage", "-i", "../server", "-n", "peergos",
                        "--main-class", "peergos.server.Main", "--main-jar",
                        "Peergos.jar", "--vendor", "Peergos Ltd.",
