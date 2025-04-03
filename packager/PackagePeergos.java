7197988ee3afeddf938f1e7918ca3b77f42cc1e5
ian <ianopolous@protonmail.com>
2025-04-03 11:26:48 +0100

    bump desktop app version

--- packager/PackagePeergos.java
+++ packager/PackagePeergos.java
@@ -9,7 +9,7 @@ import java.util.zip.*;
  *  For building rpms on ubuntu install the rpm package
  */
 public class PackagePeergos {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     public static void main(String[] a) throws Exception {
         String OS = canonicaliseOS(System.getProperty("os.name").toLowerCase());
