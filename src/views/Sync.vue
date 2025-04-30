7d4cc9e30591e4bb5c08a163837b0e91266c82be
ian <ianopolous@protonmail.com>
2025-04-30 10:02:01 +0100

    wip sync ui

---
+++ src/views/Sync.vue
@@ -0,0 +1,181 @@
+<template>
+   	<article class="app-view sync-view">
+	   	<AppHeader>
+			<template #primary>
+				<h1>{{ translate("SYNC.TITLE") }}</h1>
+			</template>
+		</AppHeader>
+		<main>
+
+            <div>
+                <div class="hspace-5">
+		    <button class="btn btn-success" @click="addSyncPair()">{{ translate("SYNC.ADDPAIR") }}</button>
+                </div>
+                <table class="drive-table">
+                <thead><tr>
+                    <th>{{ translate("SYNC.LOCAL") }}</th>
+                    <th>{{ translate("SYNC.REMOTE") }}</th>
+                    <th></th>
+                </tr></thead>
+                <tbody>
+                   <tr v-for="pair in syncPairs" tabindex="1" role="row" class="table__item">
+                      <td>{{ pair.localpath }}</td>
+                      <td>{{ pair.remotepath }}</td>
+                      <td><button class="btn btn-success" @click="removeSyncPair(pair.label)">{{ translate("SYNC.STOPPAIR") }}</button></td>
+                   </tr>
+                </tbody>
+                </table>
+            </div>
+            <input
+               type="file"
+                id="uploadDirectoriesInput"
+                @change="uploadFiles"
+                style="display: none"
+                multiple
+                directory
+                mozDirectory
+                webkitDirectory
+            />
+        </main>
+   </article>
+</template>
+
+<script>
+const AppHeader = require("../components/AppHeader.vue");
+const i18n = require("../i18n/index.js");
+
+const routerMixins = require("../mixins/router/index.js");
+
+module.exports = {
+	components: {
+		AppHeader,
+	},
+    data() {
+        return {
+            syncPairs: []
+        }
+    },
+    props: [],
+	mixins:[routerMixins, i18n],
+
+	computed: {
+		...Vuex.mapState([
+			'context',
+		]),
+		...Vuex.mapGetters([
+			'getPath'
+		]),
+    },
+	created() {
+	    let that = this;
+            this.getSyncState();
+    },
+    methods: {
+		...Vuex.mapActions([
+			'updateSocial'
+		]),
+        localPost(url, body) {
+            return new Promise(function(resolve, reject) {
+	    var req = new XMLHttpRequest();
+	    req.open('POST', url);
+            req.responseType = 'json';
+	    
+	    req.onload = function() {
+                // This is called even on 404 etc
+                // so check the status
+                if (req.status == 200) {
+		    resolve(JSON.parse(req.response));
+                }
+                else {
+		    try {
+                        let trailer = req.getResponseHeader("Trailer");
+                        if (trailer == null) {
+                            reject('Unexpected error from server');
+                        } else {
+                            reject(trailer);
+                        }
+		    } catch (e) {
+		        reject(e);
+		    }
+                }
+	    };
+	    
+	    req.onerror = function(e) {
+                future.completeExceptionally(new java.net.ConnectException("Unable to connect"));
+	    };
+            
+            req.ontimeout = function() {
+                reject(Error("Network timeout"));
+            };
+            
+	    req.send(body != null ? body : new Int8Array(0));
+        })
+        },
+
+        getSyncState() {
+            var that = this;
+            localPost("/peergos/v0/config/sync/get-pairs").then(function(result, err) {
+               that.syncPairs = result.pairs;
+            })
+        },
+
+        getHostDir() { // returns Promise<String>
+           
+        },
+
+        getPeergosDir() { // returns Promise<Path>
+           
+        },
+
+        addSyncPair() {
+            const that = this;
+            const hostDir = await getHostDir();
+            const peergosDir = await getPeergosDir();
+            if (peergosDir.substring(1).split("/").length < 2) {
+               throw "You cannot sync to your home dir, please make a sub folder";
+            }
+            that.context.sharWriteAccessWith(peergosDir, peergos.client.JsUtil.asSet([])).thenCompose(done => {
+               return that context.createSecretLink(peergosDir.toString(), true, java.util.Optional.empty(), java.util.Optional.empty(), "", false)
+            }).thenCompose(link => {
+               const cap = link.toLinkString(that.context.signer.publicKeyHash)
+               const label = cap.substring(cap.lastIndexOf("/", cap.indexOf("#")) + 1, cap.indexOf("#"))
+               localPost("/peergos/v0/config/sync/remove-pair?label="+label).then(function(result, err) {
+                   if (err != null)
+                      return
+                  that.syncPairs.add({localpath:hostDir, remotepath:peergosDir.toString(), label:label});
+               })
+            });
+        },
+
+        removeSyncPair(label) {
+            var that = this;
+            localPost("/peergos/v0/config/sync/remove-pair?label="+label).then(function(result, err) {
+               if (err != null)
+                   return
+               var index = 0;
+               for (; index< that.syncPairs.length; index++) {
+                   if (that.syncPairs[index].label == label)
+                       break;
+               }
+               that.syncPairs.splice(index, 1);
+            })
+        },
+
+        close () {
+            this.$emit("hide-sync");
+        }
+    },
+
+}
+</script>
+
+<style>
+.sync-view main{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    min-height: 100vh;
+    padding: var(--app-margin);
+}
+
+</style>
