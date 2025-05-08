b9d46ba59c82e362bdb75ac7cfbce3241170a46d
kevodwyer <kevodwyer@gmail.com>
2025-05-08 17:48:00 +0100

    add simple folder picker

---
+++ src/components/picker/SimpleFolderPicker.vue
@@ -0,0 +1,169 @@
+<template>
+<transition name="modal">
+<div class="modal-mask" @click="close">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <div @click.stop class="folder-picker-container">
+        <span @click="close" tabindex="0" v-on:keyup.enter="close" aria-label="close" class="close">&times;</span>
+        <div class="modal-header">
+            <h2>Folder Picker</h2>
+        </div>
+        <div class="modal-body">
+            <Spinner v-if="showSpinner" :message="spinnerMessage"></Spinner>
+            <div class="folder-picker-view" class="scroll-style">
+              <ul>
+                <SimpleTreeItem class="item" :model="treeData" :selectFolder_func="selectFolder"></SimpleTreeItem>
+              </ul>
+            </div>
+            <h4>Selected:</h4>
+            <div v-if="selectedFoldersList.length == 0 && multipleFolderSelection">
+            {{ translate("FOLDER.PICKER.NO.FOLDERS") }}
+            </div>
+            <div v-if="selectedFoldersList.length == 0 && !multipleFolderSelection">
+            {{ translate("FOLDER.PICKER.NO.FOLDER") }}
+            </div>
+            <div v-if="selectedFoldersList.length != 0">
+                <div class="selected-folders-view" class="scroll-style">
+                    <ul>
+                        <li v-for="selectedFolder in selectedFoldersList">
+                            {{ selectedFolder }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="flex-line-item">
+                <div>
+                    <button class="btn btn-success" style = "width:80%" @click="foldersSelected()">Done</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</transition>
+</template>
+
+<script>
+
+const Spinner = require("../spinner/Spinner.vue");
+const SimpleTreeItem = require("SimpleTreeItem.vue");
+const i18n = require("../../i18n/index.js");
+module.exports = {
+    components: {
+        Spinner,
+        SimpleTreeItem,
+        i18n
+    },
+    data: function() {
+        return {
+            showSpinner: true,
+            spinnerMessage: 'Loading folders...',
+            treeData: {},
+            selectedFoldersList: []
+        }
+    },
+    props: ['baseFolder', 'selectedFolder_func','preloadFolders_func','multipleFolderSelection'],
+    mixins:[i18n],
+    computed: {
+        ...Vuex.mapState([
+            'context',
+        ]),
+    },
+    created: function() {
+        let that = this;
+        let callback = (baseOfFolderTree) => {
+            that.treeData = baseOfFolderTree;
+            that.showSpinner = false;
+            that.spinnerMessage = '';
+        };
+        this.preloadFolders_func(this.baseFolder + "/", callback);
+    },
+    methods: {
+        close: function () {
+            this.selectedFolder_func([]);
+        },
+        showError: function(msg) {
+            console.log(msg);
+            this.$toast.error(msg, {timeout:false});
+        },
+        selectFolder: function (folderName, add) {
+            if (add) {
+                if (this.multipleFolderSelection) {
+                    this.selectedFoldersList.push(folderName);
+                } else {
+                    if (this.selectedFoldersList.length > 0) {
+                        this.showError(this.translate("FOLDER.PICKER.MULTIPLE.SELECTION.NOT.SUPPORTED"));
+                        return false;
+                    } else {
+                        this.selectedFoldersList.push(folderName);
+                    }
+                }
+            } else {
+                let index = this.selectedFoldersList.findIndex(v => v === folderName);
+                if (index > -1) {
+                    this.selectedFoldersList.splice(index, 1);
+                }
+            }
+            return true;
+        },
+        foldersSelected: function() {
+            let selectedFolders = this.selectedFoldersList;
+            // remove duplicates (one folder includes another)
+            let dedupList = [];
+            for(var i = 0; i < selectedFolders.length; i++) {
+                let folder = selectedFolders[i] + '/';
+                var isDuplicated = false;
+                for(var j = 0; j < selectedFolders.length; j++) {
+                    if (i != j) {
+                        let anotherFolder = selectedFolders[j];
+                        if (anotherFolder.startsWith(folder)) {
+                            isDuplicated = true;
+                            break;
+                        }
+                    }
+                }
+                if (! isDuplicated) {
+                    dedupList.push(selectedFolders[i]);
+                }
+            }
+            this.selectedFolder_func(dedupList);
+        }
+    }
+}
+</script>
+
+<style>
+.folder-picker-container {
+    height: 100%;
+    width: 600px;
+    overflow-y: auto;
+    position: fixed;
+    left: 50%;
+    transform: translate(-50%, 0);
+    padding: 20px 30px;
+    background-color: var(--bg);
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0,0,0,.33);
+    transition: all .3s ease;
+}
+.folder-picker-view {
+    font-size: 1.3em;
+}
+.selected-folders-view {
+    max-height: 250px;
+    overflow-y: scroll;
+    border: 2px solid var(--green-500);
+    margin: 8px 0;
+}
+.item {
+  cursor: pointer;
+  line-height: 1.5;
+}
+.bold {
+  font-weight: bold;
+}
+.scroll-style {
+    max-height: 250px;
+    overflow-y: scroll;
+    border: 2px solid var(--green-500);
+    margin: 8px 0;
+}
+</style>
--- src/views/Sync.vue
+++ src/views/Sync.vue
@@ -26,13 +26,13 @@
                 </tbody>
                 </table>
             </div>
-            <FolderPicker
+            <SimpleFolderPicker
               v-if="showFolderPicker"
-              :baseFolder="folderPickerBaseFolder" :selectedFolder_func="selectedFoldersFromPicker"
-              :multipleFolderSelection="multipleFolderSelection"
-              :initiallySelectedPaths="initiallySelectedPaths"
-              :noDriveSelection="true">
-            </FolderPicker>
+              :baseFolder="folderPickerBaseFolder"
+              :selectedFolder_func="selectedFoldersFromPicker"
+              :preloadFolders_func="preloadFolders"
+              :multipleFolderSelection="multipleFolderSelection">
+            </SimpleFolderPicker>
             <!--<input
                type="file"
                 id="uploadDirectoriesInput"
@@ -49,7 +49,7 @@
 
 <script>
 const AppHeader = require("../components/AppHeader.vue");
-const FolderPicker = require('../components/picker/FolderPicker.vue');
+const SimpleFolderPicker = require('../components/picker/SimpleFolderPicker.vue');
 
 const i18n = require("../i18n/index.js");
 
@@ -58,7 +58,7 @@ const routerMixins = require("../mixins/router/index.js");
 module.exports = {
 	components: {
 		AppHeader,
-		FolderPicker,
+		SimpleFolderPicker,
 	},
     data() {
         return {
@@ -213,6 +213,60 @@ module.exports = {
                 that.showFolderPicker = false;
             };
             this.showFolderPicker = true;
+            return future;
+        },
+        preloadFolders: function(path, callback) {
+            var that = this;
+            let folderTree = {};
+            this.context.getByPath(path).thenApply(function(dirOpt){
+                let dir = dirOpt.get();
+                let folderProperties = dir.getFileProperties();
+                if (folderProperties.isDirectory && !folderProperties.isHidden) {
+                    that.walkFolder(dir, path, folderTree).thenApply( () => {
+                        callback(folderTree);
+                    });
+                } else {
+                    callback(folderTree);
+                }
+            }).exceptionally(function(throwable) {
+                this.spinnerMessage = 'Unable to preload folders...';
+                throwable.printStackTrace();
+            });
+        },
+
+        walkFolder: function(file, path, currentTreeData) {
+            currentTreeData.path = path.substring(0, path.length -1);
+            currentTreeData.children = [];
+            let that = this;
+            let future = peergos.shared.util.Futures.incomplete();
+            file.getChildren(that.context.crypto.hasher, that.context.network).thenApply(function(children) {
+                let arr = children.toArray();
+                let funcArray = [];
+                arr.forEach(function(child, index){
+                    let childProps = child.getFileProperties();
+                    let newPath = childProps.isDirectory ? path + child.getFileProperties().name + '/' : path;
+                    if (childProps.isDirectory && !childProps.isHidden) {
+                        let node = {};
+                        currentTreeData.children.push(node);
+                        funcArray.push(() => {
+                            return that.walkFolder(child, newPath, node);
+                        });
+                    }
+                });
+                if (funcArray.length > 0) {
+                    let completed = {count: 0};
+                    funcArray.forEach((func, idx) => {
+                        func().thenApply(() => {
+                            completed.count ++;
+                            if (completed.count == funcArray.length) {
+                                future.complete(true);
+                            }
+                        });
+                    });
+                } else {
+                    future.complete(true);
+                }
+            });
             return future;
         },
     },
