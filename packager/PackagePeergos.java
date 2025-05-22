eeb9b8af387153f48673029bad66c9a53e9902e7
ian <ianopolous@protonmail.com>
2025-05-22 13:23:27 +0100

    v1.3.0

--- packager/PackagePeergos.java
+++ packager/PackagePeergos.java
@@ -9,7 +9,7 @@ import java.util.zip.*;
  *  For building rpms on ubuntu install the rpm package
  */
 public class PackagePeergos {
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 
     public static void main(String[] a) throws Exception {
         String OS = canonicaliseOS(System.getProperty("os.name").toLowerCase());
