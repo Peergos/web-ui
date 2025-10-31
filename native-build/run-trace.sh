a01a303ecbaa815435b8748c2e34b64e9eba69ad
ian <ianopolous@protonmail.com>
2025-10-31 15:14:10 +0000

    upgrade native image to jdk 25

--- native-build/run-trace.sh
+++ native-build/run-trace.sh
@@ -1,6 +1,6 @@
 
 # See https://www.graalvm.org/latest/reference-manual/native-image/metadata/AutomaticMetadataCollection/
-export PATH=/home/ian/web-ui/native-build/graalvm-community-openjdk-22+36.1/bin:$PATH
+export PATH=/home/ian/web-ui/native-build/graalvm-community-openjdk-25.0.1+8.1/bin:$PATH
 
 java -agentlib:native-image-agent=config-output-dir=META-INF/native-image -jar Peergos.jar daemon -PEERGOS_PATH /home/ian/web-ui/native-build/.peergos -log-to-console true
 
