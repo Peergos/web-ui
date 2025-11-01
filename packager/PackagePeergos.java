f3bee6a8ea04c824f2d47da043c12666f044c2c5
ian <ianopolous@protonmail.com>
2025-11-01 20:39:41 +0000

    user arm64 not aarch64 for deb

--- packager/PackagePeergos.java
+++ packager/PackagePeergos.java
@@ -30,6 +30,8 @@ public class PackagePeergos {
         boolean isDeb = type.equals("deb");
         // For debian we build using teh static image compiled from native-image
         if (isDeb) {
+            if (ARCH.equals("aarch64"))
+                ARCH = "arm64";
             new File("peergos/bin").mkdirs();
             Files.copy(Paths.get("../native-build/peergos"), Paths.get("peergos/bin/peergos"), StandardCopyOption.REPLACE_EXISTING);
             List<Path> sharedLibraries = Files.walk(Paths.get("../native-build"), 1)
