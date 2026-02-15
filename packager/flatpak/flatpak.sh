ed9182cfc922875e3c935c663ac92ace124e69fe
ian <ianopolous@protonmail.com>
2026-02-15 19:18:57 +0000

    filter log spam from chromium in flatpak

--- packager/flatpak/flatpak.sh
+++ packager/flatpak/flatpak.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-chromium --no-default-browser-check --disable-sync --disable-default-apps --no-first-run --window-size=1280,900 --log-level=3 --app=http://localhost:$1
+chromium --no-default-browser-check --disable-sync --disable-default-apps --no-first-run --window-size=1280,900 --log-level=3 --app=http://localhost:$1 2>&1 | grep -v "xapp-gtk3-module"
