681f470378de87397c829196445e555d19e11a0c
Ian Preston <ianopolous@protonmail.com>
2026-08-30 17:37:48 +0100

    include the peergos version alongside the OS in user feedback

--- src/mixins/feedback/index.js
+++ src/mixins/feedback/index.js
@@ -8,14 +8,20 @@ module.exports = {
             return "Linux";
         },
 
-        // Sends user feedback with the email follow-up consent and OS appended,
+        // Sends user feedback with the email follow-up consent, OS and Peergos version appended,
         // trimmed to fit the maximum server message size
         sendUserFeedback: function(context, feedback, allowEmailFollowup) {
-            var maxContentSize = peergos.shared.user.ServerMessage.MAX_CONTENT_SIZE;
-            var suffix = (allowEmailFollowup ? "\nemail: yes" : "") + "\nOS: " + this.detectOs();
-            var maxFeedbackSize = maxContentSize - suffix.length;
-            var trimmedContents = (feedback.length > maxFeedbackSize ? feedback.substring(0, maxFeedbackSize) : feedback) + suffix;
-            return context.sendFeedback(trimmedContents);
+            let that = this;
+            return context.getVersion()
+                .exceptionally(e => "unknown")
+                .thenCompose(version => {
+                    var maxContentSize = peergos.shared.user.ServerMessage.MAX_CONTENT_SIZE;
+                    var suffix = (allowEmailFollowup ? "\nemail: yes" : "") + "\nOS: " + that.detectOs()
+                        + "\nVersion: " + version;
+                    var maxFeedbackSize = maxContentSize - suffix.length;
+                    var trimmedContents = (feedback.length > maxFeedbackSize ? feedback.substring(0, maxFeedbackSize) : feedback) + suffix;
+                    return context.sendFeedback(trimmedContents);
+                });
         }
     }
 }
